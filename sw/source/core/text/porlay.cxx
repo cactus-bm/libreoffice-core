LeeHsiang1/core

sw_redlinehide: make SwScanner independent of SwTextNode
Abstract out the GetLang function into a parameter.

SwScanner also uses a ModelToViewHelper but at this place it's used in
"pass-through" mode which does an identity mapping.

Change-Id: I45557c4e4446d1b4d95a206c414bbb6477593f8a

sw/source/core/text/porlay.cxx
@@ -1080,9 +1080,22 @@ void SwScriptInfo::InitScriptInfo(const SwTextNode& rNode,
         // we search for connecting opportunities (kashida)
         else if ( bAdjustBlock && i18n::ScriptType::COMPLEX == nScript )
         {
-            SwScanner aScanner( rNode, rText, nullptr, ModelToViewHelper(),
+            // sw_redlinehide: this is the only place that uses SwScanner with
+            // frame text, so we convert to sal_Int32 here
+            std::function<LanguageType (sal_Int32, sal_Int32, bool)> const pGetLangOfCharM(
+                [&pMerged](sal_Int32 const nBegin, sal_uInt16 const nScript, bool const bNoChar)
+                    {
+                        std::pair<SwTextNode const*, sal_Int32> const pos(
+                            sw::MapViewToModel(*pMerged, TextFrameIndex(nBegin)));
+                        return pos.first->GetLang(pos.second, bNoChar ? 0 : 1, nScript);
+                    });
+            std::function<LanguageType (sal_Int32, sal_Int32, bool)> const pGetLangOfChar1(
+                [&rNode](sal_Int32 const nBegin, sal_uInt16 const nScript, bool const bNoChar)
+                    { return rNode.GetLang(nBegin, bNoChar ? 0 : 1, nScript); });
+            auto pGetLangOfChar(pMerged ? pGetLangOfCharM : pGetLangOfChar1);
+            SwScanner aScanner( pGetLangOfChar, rText, nullptr, ModelToViewHelper(),
                                 i18n::WordType::DICTIONARY_WORD,
-                                nLastKashida, nChg );
+                                sal_Int32(nLastKashida), sal_Int32(nChg));
 
             // the search has to be performed on a per word base
             while ( aScanner.NextWord() )
@@ -1235,7 +1248,7 @@ void SwScriptInfo::InitScriptInfo(const SwTextNode& rNode,
 
                 if ( -1 != nKashidaPos )
                 {
-                    m_Kashida.insert(m_Kashida.begin() + nCntKash, nKashidaPos);
+                    m_Kashida.insert(m_Kashida.begin() + nCntKash, TextFrameIndex(nKashidaPos));
                     nCntKash++;
                 }
             } // end of kashida search

cactus-bm/libreoffice-core

sw/source/core/text/porlay.cxx
@@ -1080,9 +1080,22 @@ void SwScriptInfo::InitScriptInfo(const SwTextNode& rNode,
         // we search for connecting opportunities (kashida)
         else if ( bAdjustBlock && i18n::ScriptType::COMPLEX == nScript )
         {
-            SwScanner aScanner( rNode, rText, nullptr, ModelToViewHelper(),
+            // sw_redlinehide: this is the only place that uses SwScanner with
+            // frame text, so we convert to sal_Int32 here
+            std::function<LanguageType (sal_Int32, sal_Int32, bool)> const pGetLangOfCharM(
+                [&pMerged](sal_Int32 const nBegin, sal_uInt16 const script, bool const bNoChar)
+                    {
+                        std::pair<SwTextNode const*, sal_Int32> const pos(
+                            sw::MapViewToModel(*pMerged, TextFrameIndex(nBegin)));
+                        return pos.first->GetLang(pos.second, bNoChar ? 0 : 1, script);
+                    });
+            std::function<LanguageType (sal_Int32, sal_Int32, bool)> const pGetLangOfChar1(
+                [&rNode](sal_Int32 const nBegin, sal_uInt16 const script, bool const bNoChar)
+                    { return rNode.GetLang(nBegin, bNoChar ? 0 : 1, script); });
+            auto pGetLangOfChar(pMerged ? pGetLangOfCharM : pGetLangOfChar1);
+            SwScanner aScanner( pGetLangOfChar, rText, nullptr, ModelToViewHelper(),
                                 i18n::WordType::DICTIONARY_WORD,
-                                nLastKashida, nChg );
+                                sal_Int32(nLastKashida), sal_Int32(nChg));
 
             // the search has to be performed on a per word base
             while ( aScanner.NextWord() )
@@ -1235,7 +1248,7 @@ void SwScriptInfo::InitScriptInfo(const SwTextNode& rNode,
 
                 if ( -1 != nKashidaPos )
                 {
-                    m_Kashida.insert(m_Kashida.begin() + nCntKash, nKashidaPos);
+                    m_Kashida.insert(m_Kashida.begin() + nCntKash, TextFrameIndex(nKashidaPos));
                     nCntKash++;
                 }
             } // end of kashida search