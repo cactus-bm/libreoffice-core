LibreOffice/core

commit 1f08bff31238d5818c54a0b86570689644dff087

new loplugin:shouldreturnbool
look for methods returning only 1 and/or 0, which (most of the time)
should be returning bool.

Off by default, because some of this is a matter of taste

Change-Id: Ib17782e629888255196e89d4a178618a9612a0de
Reviewed-on: https://gerrit.libreoffice.org/54379
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

store/source/storbios.cxx
@@ -396,13 +396,12 @@ OStorePageBIOS::Ace::~Ace()
   m_prev->m_next = m_next;
 }
 
-int
+void
 SAL_CALL OStorePageBIOS::Ace::constructor (
     void * obj, SAL_UNUSED_PARAMETER void * /* arg */)
 {
   Ace * ace = static_cast<Ace*>(obj);
   ace->m_next = ace->m_prev = ace;
-  return 1;
 }
 
 OStorePageBIOS::Ace *

store/source/storbios.hxx
@@ -141,7 +141,7 @@ public:
         Ace();
         ~Ace();
 
-        static int SAL_CALL constructor (void * obj, void * arg);
+        static void SAL_CALL constructor (void * obj, void * arg);
 
         static Ace * find   (Ace * head, sal_uInt32 addr);
         static void  insert (Ace * head, Ace * entry);

chart2/source/view/main/OpenGLRender.cxx
@@ -169,7 +169,7 @@ int OpenGLRender::InitOpenGL()
     return 0;
 }
 
-int OpenGLRender::SetLine2DShapePoint(float x, float y, int listLength)
+void OpenGLRender::SetLine2DShapePoint(float x, float y, int listLength)
 {
     if (m_Line2DPointList.empty())
     {
@@ -184,7 +184,6 @@ int OpenGLRender::SetLine2DShapePoint(float x, float y, int listLength)
         m_Line2DShapePointList.push_back(m_Line2DPointList);
         m_Line2DPointList.clear();
     }
-    return 0;
 }
 
 int OpenGLRender::RenderLine2FBO()
@@ -275,13 +274,12 @@ void OpenGLRender::prepareToRender()
     m_fZStep = 0;
 }
 
-int OpenGLRender::MoveModelf(const PosVecf3& trans, const PosVecf3& angle, const PosVecf3& scale)
+void OpenGLRender::MoveModelf(const PosVecf3& trans, const PosVecf3& angle, const PosVecf3& scale)
 {
     glm::mat4 aTranslationMatrix = glm::translate(glm::vec3(trans.x, trans.y, trans.z));
     glm::mat4 aScaleMatrix = glm::scale(glm::vec3(scale.x, scale.y, scale.z));
     glm::mat4 aRotationMatrix = glm::eulerAngleYXZ(angle.y, angle.x, angle.z);
     m_Model = aTranslationMatrix * aRotationMatrix * aScaleMatrix;
-    return 0;
 }
 
 void OpenGLRender::Release()
@@ -392,7 +390,7 @@ int OpenGLRender::Create2DCircle(int detail)
     return 0;
 }
 
-int OpenGLRender::Bubble2DShapePoint(float x, float y, float directionX, float directionY)
+void OpenGLRender::Bubble2DShapePoint(float x, float y, float directionX, float directionY)
 {
     //check whether to create the circle data
     if (m_Bubble2DCircle.empty())
@@ -407,7 +405,6 @@ int OpenGLRender::Bubble2DShapePoint(float x, float y, float directionX, float d
     aBubble2DPointList.y = y + aBubble2DPointList.yScale / 2;
 
     m_Bubble2DShapePointList.push_back(aBubble2DPointList);
-    return 0;
 }
 
 int OpenGLRender::RenderBubble2FBO()
@@ -487,7 +484,7 @@ int OpenGLRender::RenderBubble2FBO()
     return 0;
 }
 
-int OpenGLRender::RectangleShapePoint(float x, float y, float directionX, float directionY)
+void OpenGLRender::RectangleShapePoint(float x, float y, float directionX, float directionY)
 {
     RectanglePointList aRectangle;
 
@@ -505,10 +502,9 @@ int OpenGLRender::RectangleShapePoint(float x, float y, float directionX, float 
     aRectangle.points[11] = m_fZStep;
 
     m_RectangleShapePointList.push_back(aRectangle);
-    return 0;
 }
 
-int OpenGLRender::RenderRectangleShape(bool bBorder, bool bFill)
+void OpenGLRender::RenderRectangleShape(bool bBorder, bool bFill)
 {
     for (auto const& pointList : m_RectangleShapePointList)
     {
@@ -613,7 +609,6 @@ int OpenGLRender::RenderRectangleShape(bool bBorder, bool bFill)
     CHECK_GL_ERROR();
 
     m_fZStep += Z_STEP;
-    return 0;
 }
 
 
@@ -637,11 +632,12 @@ int OpenGLRender::CreateTextTexture(const BitmapEx& rBitmapEx, const awt::Size& 
 
     OpenGLHelper::ConvertBitmapExToRGBATextureBuffer(rBitmapEx, bitmapBuf.get());
 
-    return CreateTextTexture(bitmapBuf, rBitmapEx.GetSizePixel(),
+    CreateTextTexture(bitmapBuf, rBitmapEx.GetSizePixel(),
                              aSize, rotation, rTrans);
+    return 0;
 }
 
-int OpenGLRender::CreateTextTexture(const boost::shared_array<sal_uInt8> &rPixels,
+void OpenGLRender::CreateTextTexture(const boost::shared_array<sal_uInt8> &rPixels,
                                     const ::Size &aPixelSize,
                                     const awt::Size& aSize,
                                     long rotation,
@@ -688,10 +684,9 @@ int OpenGLRender::CreateTextTexture(const boost::shared_array<sal_uInt8> &rPixel
     glBindTexture(GL_TEXTURE_2D, 0);
     CHECK_GL_ERROR();
     m_TextInfoVector.push_back(aTextInfo);
-    return 0;
 }
 
-int OpenGLRender::RenderTextShape()
+void OpenGLRender::RenderTextShape()
 {
     CHECK_GL_ERROR();
     for (auto const& textInfo : m_TextInfoVector)
@@ -753,10 +748,9 @@ int OpenGLRender::RenderTextShape()
     m_TextInfoVector.clear();
     CHECK_GL_ERROR();
     m_fZStep += Z_STEP;
-    return 0;
 }
 
-int OpenGLRender::SetArea2DShapePoint(float x, float y, int listLength)
+void OpenGLRender::SetArea2DShapePoint(float x, float y, int listLength)
 {
     if (m_Area2DPointList.empty())
     {
@@ -771,7 +765,6 @@ int OpenGLRender::SetArea2DShapePoint(float x, float y, int listLength)
         m_Area2DShapePointList.push_back(m_Area2DPointList);
         m_Area2DPointList.clear();
     }
-    return 0;
 }
 
 namespace {
@@ -799,7 +792,7 @@ bool checkCCW(const PointList& rPoints)
 
 }
 
-int OpenGLRender::RenderArea2DShape()
+void OpenGLRender::RenderArea2DShape()
 {
     CHECK_GL_ERROR();
 
@@ -847,8 +840,6 @@ int OpenGLRender::RenderArea2DShape()
     m_fZStep += Z_STEP;
 
     CHECK_GL_ERROR();
-
-    return 0;
 }
 
 void OpenGLRender::SetBackGroundColor(::Color color1, ::Color color2, css::drawing::FillStyle fillStyle)
@@ -932,7 +923,7 @@ void OpenGLRender::GeneratePieSegment2D(double fInnerRadius, double fOutterRadiu
     m_PieSegment2DShapePointList.push_back(aPointList);
 }
 
-int OpenGLRender::RenderPieSegment2DShape(float fSize, float fPosX, float fPosY)
+void OpenGLRender::RenderPieSegment2DShape(float fSize, float fPosX, float fPosY)
 {
     int listNum = m_PieSegment2DShapePointList.size();
     PosVecf3 const trans = {fPosX, fPosY, 0.0f};
@@ -975,10 +966,9 @@ int OpenGLRender::RenderPieSegment2DShape(float fSize, float fPosX, float fPosY)
     m_fZStep += Z_STEP;
 
     CHECK_GL_ERROR();
-    return 0;
 }
 
-int OpenGLRender::RenderSymbol2DShape(float x, float y, sal_Int32 nSymbol)
+void OpenGLRender::RenderSymbol2DShape(float x, float y, sal_Int32 nSymbol)
 {
     CHECK_GL_ERROR();
 
@@ -1028,7 +1018,6 @@ int OpenGLRender::RenderSymbol2DShape(float x, float y, sal_Int32 nSymbol)
     m_fZStep += Z_STEP;
 
     CHECK_GL_ERROR();
-    return 0;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

chart2/source/view/main/OpenGLRender.hxx
@@ -67,24 +67,24 @@ public:
     OpenGLRender();
     ~OpenGLRender();
     int InitOpenGL();
-    int MoveModelf(const PosVecf3& trans, const PosVecf3& angle, const PosVecf3& scale);
+    void MoveModelf(const PosVecf3& trans, const PosVecf3& angle, const PosVecf3& scale);
     void SetSize(int width, int height);
     void SetSizePixel(int width, int height);
     void Release();
     int RenderLine2FBO();
-    int SetLine2DShapePoint(float x, float y, int listLength);
+    void SetLine2DShapePoint(float x, float y, int listLength);
     void SetLine2DColor(sal_uInt8 r, sal_uInt8 g, sal_uInt8 b, sal_uInt8 nAlpha);
     void SetLine2DWidth(int width);
     void SetColor(sal_uInt32 color, sal_uInt8 nAlpha);
-    int Bubble2DShapePoint(float x, float y, float directionX, float directionY);
+    void Bubble2DShapePoint(float x, float y, float directionX, float directionY);
     int RenderBubble2FBO();
 
     void prepareToRender();
 
-    int RenderRectangleShape(bool bBorder, bool bFill);
-    int RectangleShapePoint(float x, float y, float directionX, float directionY);
+    void RenderRectangleShape(bool bBorder, bool bFill);
+    void RectangleShapePoint(float x, float y, float directionX, float directionY);
 
-    int CreateTextTexture(const boost::shared_array<sal_uInt8> &rPixels,
+    void CreateTextTexture(const boost::shared_array<sal_uInt8> &rPixels,
                           const ::Size &aPixelSize,
                           const css::awt::Size& aSize,
                           long rotation,
@@ -92,16 +92,16 @@ public:
     int CreateTextTexture(const BitmapEx& rBitmapEx,
             const css::awt::Size& aSize,
             long rotation, const css::drawing::HomogenMatrix3& rTrans);
-    int RenderTextShape();
+    void RenderTextShape();
 
-    int SetArea2DShapePoint(float x, float y, int listLength);
-    int RenderArea2DShape();
+    void SetArea2DShapePoint(float x, float y, int listLength);
+    void RenderArea2DShape();
     void SetChartTransparencyGradient(long transparencyGradient);
 
     void GeneratePieSegment2D(double, double, double, double);
-    int RenderPieSegment2DShape(float, float, float);
+    void RenderPieSegment2DShape(float, float, float);
 
-    int RenderSymbol2DShape(float, float, sal_Int32);
+    void RenderSymbol2DShape(float, float, sal_Int32);
 #if DEBUG_POSITIONING
     void renderDebug();
 #endif

compilerplugins/clang/shouldreturnbool.cxx
@@ -1,0 +1,250 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <string>
+#include <set>
+#include <iostream>
+
+#include "check.hxx"
+#include "plugin.hxx"
+#include "functionaddress.hxx"
+
+/*
+ Look for functions that only return 1 and/or 0, which sometimes indicates that the
+ function should be returning bool.
+
+ Note that is partly a question of taste and code style, which is why this plugin is off by default.
+*/
+
+namespace
+{
+class ShouldReturnBool : public loplugin::FunctionAddress<ShouldReturnBool>
+{
+public:
+    explicit ShouldReturnBool(loplugin::InstantiationData const& data)
+        : loplugin::FunctionAddress<ShouldReturnBool>(data)
+    {
+    }
+
+    virtual void run() override
+    {
+        if (!compiler.getLangOpts().CPlusPlus)
+            return;
+        StringRef fn(compiler.getSourceManager()
+                         .getFileEntryForID(compiler.getSourceManager().getMainFileID())
+                         ->getName());
+        // functions used as function pointers
+        if (loplugin::isSamePathname(fn, SRCDIR "/sal/rtl/alloc_cache.cxx"))
+            return;
+        // false +, slightly odd usage, but not wrong
+        if (loplugin::isSamePathname(fn, SRCDIR "/libreofficekit/qa/tilebench/tilebench.cxx")
+            || loplugin::isSamePathname(fn, SRCDIR "/smoketest/libtest.cxx"))
+            return;
+        // uses the Unix convention of "non-zero return indicates error"
+        if (loplugin::isSamePathname(fn, SRCDIR "/idlc/source/idlcproduce.cxx"))
+            return;
+        // template magic
+        if (loplugin::isSamePathname(fn, SRCDIR "/vcl/source/gdi/bmpfast.cxx"))
+            return;
+        // fine
+        if (loplugin::isSamePathname(fn, SRCDIR "/svl/unx/source/svdde/ddedummy.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/vcl/source/opengl/OpenGLHelper.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/svtools/source/misc/imap2.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/svx/source/dialog/docrecovery.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/hwpfilter/source/lexer.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/hwpfilter/source/grammar.cxx"))
+            return;
+        if (loplugin::isSamePathname(
+                fn, SRCDIR "/connectivity/source/drivers/odbc/ODatabaseMetaDataResultSet.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/dbaccess/source/ui/browser/dsEntriesNoExp.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/lotuswordpro/source/filter/explode.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/filter/source/graphicfilter/ipict/ipict.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/sc/source/core/data/dptabsrc.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/sc/source/ui/docshell/docsh3.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/sd/source/ui/dlg/masterlayoutdlg.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/sd/source/filter/ppt/pptinanimations.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/vcl/unx/generic/app/i18n_im.cxx"))
+            return;
+
+        // callback
+        if (loplugin::isSamePathname(fn, SRCDIR "/sax/source/expatwrap/sax_expat.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/xmlsecurity/source/xmlsec/xmlstreamio.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/sw/source/filter/ww8/ww8par.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/sw/source/filter/ww8/ww8par2.cxx"))
+            return;
+        if (loplugin::isSamePathname(fn, SRCDIR "/sw/source/filter/ww8/ww8par5.cxx"))
+            return;
+        // SaxWriterHelper::writeSequence a little weird
+        if (loplugin::isSamePathname(fn, SRCDIR "/sax/source/expatwrap/saxwriter.cxx"))
+            return;
+        // main function
+        if (loplugin::isSamePathname(fn, SRCDIR "/xmlsecurity/workben/pdfverify.cxx"))
+            return;
+
+        TraverseDecl(compiler.getASTContext().getTranslationUnitDecl());
+
+        for (auto functionDecl : problemFunctions)
+        {
+            auto canonicalDecl = functionDecl->getCanonicalDecl();
+            if (getFunctionsWithAddressTaken().find(canonicalDecl)
+                != getFunctionsWithAddressTaken().end())
+                continue;
+            report(DiagnosticsEngine::Warning,
+                   "only returning one or zero is an indication you want to return bool",
+                   functionDecl->getLocStart())
+                << functionDecl->getSourceRange();
+            if (canonicalDecl->getLocation() != functionDecl->getLocation())
+            {
+                report(DiagnosticsEngine::Note, "canonical function declaration here",
+                       canonicalDecl->getLocStart())
+                    << canonicalDecl->getSourceRange();
+            }
+        }
+    }
+
+    bool TraverseFunctionDecl(FunctionDecl*);
+    bool TraverseCXXMethodDecl(CXXMethodDecl*);
+    bool VisitReturnStmt(ReturnStmt const*);
+
+private:
+    bool mbInsideFunction = false;
+    bool mbFunctionOnlyReturningOneOrZero = false;
+    std::unordered_set<FunctionDecl const*> problemFunctions;
+
+    bool IsInteresting(FunctionDecl const*);
+    void Report(FunctionDecl const*) const;
+    bool isExprOneOrZero(Expr const*) const;
+};
+
+bool ShouldReturnBool::TraverseFunctionDecl(FunctionDecl* functionDecl)
+{
+    bool ret;
+    if (IsInteresting(functionDecl))
+    {
+        mbInsideFunction = true;
+        mbFunctionOnlyReturningOneOrZero = true;
+        ret = loplugin::FunctionAddress<ShouldReturnBool>::TraverseFunctionDecl(functionDecl);
+        mbInsideFunction = false;
+        if (mbFunctionOnlyReturningOneOrZero)
+            problemFunctions.insert(functionDecl);
+    }
+    else
+        ret = loplugin::FunctionAddress<ShouldReturnBool>::TraverseFunctionDecl(functionDecl);
+    return ret;
+}
+
+bool ShouldReturnBool::TraverseCXXMethodDecl(CXXMethodDecl* methodDecl)
+{
+    bool ret;
+    if (IsInteresting(methodDecl))
+    {
+        mbInsideFunction = true;
+        mbFunctionOnlyReturningOneOrZero = true;
+        ret = loplugin::FunctionAddress<ShouldReturnBool>::TraverseCXXMethodDecl(methodDecl);
+        mbInsideFunction = false;
+        if (mbFunctionOnlyReturningOneOrZero)
+            problemFunctions.insert(methodDecl);
+    }
+    else
+        ret = loplugin::FunctionAddress<ShouldReturnBool>::TraverseCXXMethodDecl(methodDecl);
+    return ret;
+}
+
+bool ShouldReturnBool::IsInteresting(FunctionDecl const* functionDecl)
+{
+    if (ignoreLocation(functionDecl))
+        return false;
+    // ignore stuff that forms part of the stable URE interface
+    if (isInUnoIncludeFile(functionDecl))
+        return false;
+    if (functionDecl->getTemplatedKind() != FunctionDecl::TK_NonTemplate)
+        return false;
+    if (!functionDecl->isThisDeclarationADefinition())
+        return false;
+    if (functionDecl->isMain())
+        return false;
+    if (functionDecl->isExternC() || functionDecl->isInExternCContext())
+        return false;
+    auto methodDecl = dyn_cast<CXXMethodDecl>(functionDecl);
+    if (methodDecl && methodDecl->isVirtual())
+        return false;
+    auto tc = loplugin::TypeCheck(functionDecl->getReturnType());
+    if (tc.AnyBoolean() || tc.Void())
+        return false;
+    auto returnType = functionDecl->getReturnType();
+    if (returnType->isEnumeralType() || !returnType->getUnqualifiedDesugaredType()->isIntegerType())
+        return false;
+    // Ignore functions that contains #ifdef-ery
+    if (containsPreprocessingConditionalInclusion(functionDecl->getSourceRange()))
+        return false;
+
+    // not sure what basegfx is doing here
+    StringRef fileName{ compiler.getSourceManager().getFilename(functionDecl->getLocation()) };
+    if (loplugin::isSamePathname(fileName, SRCDIR "/include/basegfx/range/basicrange.hxx"))
+        return false;
+    // false +
+    if (loplugin::isSamePathname(fileName, SRCDIR "/include/svl/macitem.hxx"))
+        return false;
+    if (loplugin::isSamePathname(fileName, SRCDIR "/lotuswordpro/source/filter/lwpcharsetmgr.hxx"))
+        return false;
+    if (loplugin::isSamePathname(fileName, SRCDIR "/sc/inc/dptabsrc.hxx"))
+        return false;
+
+    return true;
+}
+
+bool ShouldReturnBool::VisitReturnStmt(const ReturnStmt* returnStmt)
+{
+    if (!mbInsideFunction)
+        return true;
+    if (!returnStmt->getRetValue())
+        return true;
+    if (loplugin::TypeCheck(returnStmt->getRetValue()->getType()).AnyBoolean())
+        return true;
+    if (!isExprOneOrZero(returnStmt->getRetValue()))
+        mbFunctionOnlyReturningOneOrZero = false;
+    return true;
+}
+
+bool ShouldReturnBool::isExprOneOrZero(const Expr* arg) const
+{
+    arg = arg->IgnoreParenCasts();
+    // ignore this, it seems to trigger an infinite recursion
+    if (isa<UnaryExprOrTypeTraitExpr>(arg))
+    {
+        return false;
+    }
+    APSInt x1;
+    if (arg->EvaluateAsInt(x1, compiler.getASTContext()))
+    {
+        return x1 == 1 || x1 == 0;
+    }
+    return false;
+}
+
+loplugin::Plugin::Registration<ShouldReturnBool> X("shouldreturnbool", false);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

compilerplugins/clang/test/shouldreturnbool.cxx
@@ -1,0 +1,31 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+int foo1(char ch)
+{
+    // expected-error@-2 {{only returning one or zero is an indication you want to return bool [loplugin:shouldreturnbool]}}
+    if (ch == 'x')
+        return 1;
+    return 0;
+}
+
+long foo2()
+{
+    // expected-error@-2 {{only returning one or zero is an indication you want to return bool [loplugin:shouldreturnbool]}}
+    return 1;
+}
+
+enum Enum1
+{
+    NONE
+};
+
+Enum1 foo3() { return Enum1::NONE; }
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

cui/source/customize/macropg.cxx
@@ -564,11 +564,12 @@ IMPL_LINK( SvxMacroTabPage_, AssignDeleteHdl_Impl, Button*, pBtn, void )
 
 IMPL_LINK_NOARG( SvxMacroTabPage_, DoubleClickHdl_Impl, SvTreeListBox*, bool)
 {
-    return GenericHandler_Impl( this, nullptr );
+    GenericHandler_Impl( this, nullptr );
+    return false;
 }
 
 // handler for double click on the listbox, and for the assign/delete buttons
-long SvxMacroTabPage_::GenericHandler_Impl( SvxMacroTabPage_* pThis, PushButton* pBtn )
+void SvxMacroTabPage_::GenericHandler_Impl( SvxMacroTabPage_* pThis, PushButton* pBtn )
 {
     SvxMacroTabPage_Impl*    pImpl = pThis->mpImpl;
     SvHeaderTabListBox& rListBox = pImpl->pEventLB->GetListBox();
@@ -576,7 +577,7 @@ long SvxMacroTabPage_::GenericHandler_Impl( SvxMacroTabPage_* pThis, PushButton*
     if( !pE || LISTBOX_ENTRY_NOTFOUND == rListBox.GetModel()->GetAbsPos( pE ) )
     {
         DBG_ASSERT( pE, "Where does the empty entry come from?" );
-        return 0;
+        return;
     }
 
     const bool bAssEnabled = pBtn != pImpl->pDeletePB && pImpl->pAssignPB->IsEnabled();
@@ -675,7 +676,6 @@ long SvxMacroTabPage_::GenericHandler_Impl( SvxMacroTabPage_* pThis, PushButton*
     rListBox.SetUpdateMode( true );
 
     pThis->EnableButtons();
-    return 0;
 }
 
 // pass in the XNameReplace.

cui/source/inc/macropg.hxx
@@ -61,7 +61,7 @@ class SvxMacroTabPage_ : public SfxTabPage
     DECL_LINK( AssignDeleteHdl_Impl, Button *, void );
     DECL_LINK( DoubleClickHdl_Impl, SvTreeListBox*, bool );
 
-    static long GenericHandler_Impl( SvxMacroTabPage_* pThis, PushButton* pBtn );
+    static void GenericHandler_Impl( SvxMacroTabPage_* pThis, PushButton* pBtn );
 
     css::uno::Reference< css::container::XNameReplace > m_xAppEvents;
 protected:

cactus-bm/libreoffice-core

commit 142d70a00422d3ed36f7fac49568e0ca9d0da115

-fsanitize=function
Regression introduced with 1f08bff31238d5818c54a0b86570689644dff087 "new
loplugin:shouldreturnbool" (and which this commit partly reverts), as
store::OStorePageBIOS::Ace::constructor is passed to rtl_cache_create in
store::OStorePageBIOS::AceCache::AceCache (store/source/storbios.cxx).

Change-Id: Ia96b456cab4832fc29b6d2abdff082b3cb6f2c79

store/source/storbios.cxx
@@ -396,12 +396,13 @@ OStorePageBIOS::Ace::~Ace()
   m_prev->m_next = m_next;
 }
 
-void
+int
 SAL_CALL OStorePageBIOS::Ace::constructor (
     void * obj, SAL_UNUSED_PARAMETER void * /* arg */)
 {
   Ace * ace = static_cast<Ace*>(obj);
   ace->m_next = ace->m_prev = ace;
+  return 1;
 }
 
 OStorePageBIOS::Ace *

store/source/storbios.hxx
@@ -141,7 +141,7 @@ public:
         Ace();
         ~Ace();
 
-        static void SAL_CALL constructor (void * obj, void * arg);
+        static int SAL_CALL constructor (void * obj, void * arg);
 
         static Ace * find   (Ace * head, sal_uInt32 addr);
         static void  insert (Ace * head, Ace * entry);