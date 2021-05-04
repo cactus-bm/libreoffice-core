LibreOffice/core

Related: tdf#140968 avoid duplicated filter values
Group and filter based on the actual cell format,
like MSO does, to simplify filtering of values rounded by
the cell format (e.g. selecting the single AutoFilter condition
"1.0" to filter both 1.01 and 0.99).

Introduce XML_FORMATTED_VALUE token to indicate the formatted filter values which
came from an OOXML file or came from a new filter condition which was a duplicated value.

Followed up of 4fd1333ba4bb4f2311e9098291154772bd310429
(tdf#140968 tdf#140978 XLSX import: fix lost rounded filters)

tdf#141775: fix regression of 4fd1333ba4bb4f2311e9098291154772bd310429
(tdf#140968 tdf#140978 XLSX import: fix lost rounded filters)

Change-Id: I1c840249ee1ef95aff33a4740b8bf2ebc47f2325

sc/qa/uitest/autofilter/autofilter.py
@@ -334,4 +334,37 @@ class AutofilterTest(UITestCase):
         xOkBtn.executeAction("CLICK", tuple())
 
         self.ui_test.close_doc()
+
+    def test_tdf140968(self):
+        doc = self.ui_test.load_file(get_url_for_data_file("tdf140968.xlsx"))
+
+        xGridWin = self.xUITest.getTopFocusWindow().getChild("grid_window")
+
+        xGridWin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "0", "ROW": "0"}))
+        xFloatWindow = self.xUITest.getFloatWindow()
+        xCheckListMenu = xFloatWindow.getChild("check_list_menu")
+        xTreeList = xCheckListMenu.getChild("check_list_box")
+        self.assertEqual(4, len(xTreeList.getChildren()))
+        self.assertEqual("0.000", get_state_as_dict(xTreeList.getChild('0'))['Text'])
+        self.assertEqual("0.046", get_state_as_dict(xTreeList.getChild('1'))['Text'])
+        self.assertEqual("0.365", get_state_as_dict(xTreeList.getChild('2'))['Text'])
+        self.assertEqual("0.516", get_state_as_dict(xTreeList.getChild('3'))['Text'])
+
+        xFirstEntry = xTreeList.getChild("0")
+        xFirstEntry.executeAction("CLICK", tuple())
+        xFirstEntry = xTreeList.getChild("1")
+        xFirstEntry.executeAction("CLICK", tuple())
+
+        xOkBtn = xFloatWindow.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+
+        self.assertFalse(is_row_hidden(doc, 0))
+        self.assertTrue(is_row_hidden(doc, 1))
+        self.assertFalse(is_row_hidden(doc, 2))
+        self.assertFalse(is_row_hidden(doc, 3))
+        self.assertFalse(is_row_hidden(doc, 4))
+        self.assertFalse(is_row_hidden(doc, 5))
+        self.assertTrue(is_row_hidden(doc, 6))
+
+        self.ui_test.close_doc()
 # vim: set shiftwidth=4 softtabstop=4 expandtab:

sc/source/filter/xml/xmlfilti.cxx
@@ -444,6 +444,9 @@ ScXMLSetItemContext::ScXMLSetItemContext(
     if ( !rAttrList.is() )
         return;
 
+    ScQueryEntry::Item aItem;
+    bool bAddSetItem = false;
+
     for (auto &aIter : *rAttrList)
     {
         switch (aIter.getToken())
@@ -451,15 +454,21 @@ ScXMLSetItemContext::ScXMLSetItemContext(
             case XML_ELEMENT( TABLE, XML_VALUE ):
             {
                 svl::SharedStringPool& rPool = GetScImport().GetDocument()->GetSharedStringPool();
-                ScQueryEntry::Item aItem;
                 aItem.maString = rPool.intern(aIter.toString());
                 aItem.meType = ScQueryEntry::ByString;
                 aItem.mfVal = 0.0;
-                rParent.AddSetItem(aItem);
+                bAddSetItem = true;
+            }
+            break;
+            case XML_ELEMENT( LO_EXT, XML_FORMATTED_VALUE ):
+            {
+                aItem.mbFormattedValue = aIter.toBoolean();
             }
             break;
         }
     }
+    if (bAddSetItem)
+        rParent.AddSetItem(aItem);
 }
 
 ScXMLSetItemContext::~ScXMLSetItemContext()

cactus-bm/libreoffice-core

tdf#141916: odf export: Indicating the formatted filter values,
by export the XML_DATA_TYPE with XML_TEXT, to avoid bad filtering
of formatted filter values.
tdf#141915: fix OOXML import of 0.0 filter values of filter conditions
by setting it byValue filtering in datauno.cxx.
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/113314
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sc/qa/uitest/autofilter/autofilter.py
@@ -334,4 +334,37 @@ class AutofilterTest(UITestCase):
         xOkBtn.executeAction("CLICK", tuple())
 
         self.ui_test.close_doc()
+
+    def test_tdf140968(self):
+        doc = self.ui_test.load_file(get_url_for_data_file("tdf140968.xlsx"))
+
+        xGridWin = self.xUITest.getTopFocusWindow().getChild("grid_window")
+
+        xGridWin.executeAction("LAUNCH", mkPropertyValues({"AUTOFILTER": "", "COL": "0", "ROW": "0"}))
+        xFloatWindow = self.xUITest.getFloatWindow()
+        xCheckListMenu = xFloatWindow.getChild("check_list_menu")
+        xTreeList = xCheckListMenu.getChild("check_list_box")
+        self.assertEqual(4, len(xTreeList.getChildren()))
+        self.assertEqual("0.000", get_state_as_dict(xTreeList.getChild('0'))['Text'])
+        self.assertEqual("0.046", get_state_as_dict(xTreeList.getChild('1'))['Text'])
+        self.assertEqual("0.365", get_state_as_dict(xTreeList.getChild('2'))['Text'])
+        self.assertEqual("0.516", get_state_as_dict(xTreeList.getChild('3'))['Text'])
+
+        xFirstEntry = xTreeList.getChild("0")
+        xFirstEntry.executeAction("CLICK", tuple())
+        xFirstEntry = xTreeList.getChild("1")
+        xFirstEntry.executeAction("CLICK", tuple())
+
+        xOkBtn = xFloatWindow.getChild("ok")
+        xOkBtn.executeAction("CLICK", tuple())
+
+        self.assertFalse(is_row_hidden(doc, 0))
+        self.assertTrue(is_row_hidden(doc, 1))
+        self.assertTrue(is_row_hidden(doc, 2))
+        self.assertTrue(is_row_hidden(doc, 3))
+        self.assertTrue(is_row_hidden(doc, 4))
+        self.assertFalse(is_row_hidden(doc, 5))
+        self.assertFalse(is_row_hidden(doc, 6))
+
+        self.ui_test.close_doc()
 # vim: set shiftwidth=4 softtabstop=4 expandtab:

sc/source/filter/xml/xmlfilti.cxx
@@ -281,7 +281,7 @@ ScXMLConditionContext::ScXMLConditionContext(
     ScXMLImportContext( rImport ),
     mrQueryParam(rParam),
     pFilterContext(pTempFilterContext),
-    sDataType(GetXMLToken(XML_TEXT)),
+    sDataType(OUString()),
     nField(0),
     bIsCaseSensitive(false)
 {
@@ -430,6 +430,8 @@ void SAL_CALL ScXMLConditionContext::endFastElement( sal_Int32 /*nElement*/ )
             svl::SharedStringPool& rPool = GetScImport().GetDocument()->GetSharedStringPool();
             rItem.maString = rPool.intern(sConditionValue);
             rItem.meType = ScQueryEntry::ByString;
+            if (IsXMLToken(sDataType, XML_TEXT))
+                rItem.mbFormattedValue = true;
         }
     }
     else
@@ -444,22 +446,31 @@ ScXMLSetItemContext::ScXMLSetItemContext(
     if ( !rAttrList.is() )
         return;
 
+    ScQueryEntry::Item aItem;
+    bool bAddSetItem = false;
+
     for (auto &aIter : *rAttrList)
     {
         switch (aIter.getToken())
         {
+            case XML_ELEMENT( TABLE, XML_DATA_TYPE ):
+            {
+                aItem.mbFormattedValue = IsXMLToken(aIter.toString(), XML_TEXT);
+            }
+            break;
             case XML_ELEMENT( TABLE, XML_VALUE ):
             {
                 svl::SharedStringPool& rPool = GetScImport().GetDocument()->GetSharedStringPool();
-                ScQueryEntry::Item aItem;
                 aItem.maString = rPool.intern(aIter.toString());
                 aItem.meType = ScQueryEntry::ByString;
                 aItem.mfVal = 0.0;
-                rParent.AddSetItem(aItem);
+                bAddSetItem = true;
             }
             break;
         }
     }
+    if (bAddSetItem)
+        rParent.AddSetItem(aItem);
 }
 
 ScXMLSetItemContext::~ScXMLSetItemContext()
@@ -649,7 +660,7 @@ ScXMLDPConditionContext::ScXMLDPConditionContext( ScXMLImport& rImport,
                                       ScXMLDPFilterContext* pTempFilterContext) :
     ScXMLImportContext( rImport ),
     pFilterContext(pTempFilterContext),
-    sDataType(GetXMLToken(XML_TEXT)),
+    sDataType(OUString()),
     nField(0),
     bIsCaseSensitive(false)
 {
@@ -762,6 +773,8 @@ void SAL_CALL ScXMLDPConditionContext::endFastElement( sal_Int32 /*nElement*/ )
             rItem.maString = rPool.intern(sConditionValue);
             rItem.meType = ScQueryEntry::ByString;
             rItem.mfVal = 0.0;
+            if (IsXMLToken(sDataType, XML_TEXT))
+                rItem.mbFormattedValue = true;
         }
     }
     pFilterContext->AddFilterField(aFilterField);