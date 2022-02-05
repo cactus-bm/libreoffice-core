LibreOffice/core

lokCalcRTL: fix rendering of charts in edit mode
Inform Sfx2InPlaceClient and LokChartHelper when negated X coordinates
are used. Ensure that invalidation rectangles have positive coordinates
in all cases.

Change-Id: I8f5440718e288d8f0d379c8da5f49a29e51f6940

sc/source/ui/view/gridwin4.cxx
@@ -33,6 +33,7 @@
 #include <LibreOfficeKit/LibreOfficeKitEnums.h>
 #include <comphelper/lok.hxx>
 #include <sfx2/lokhelper.hxx>
+#include <sfx2/lokcharthelper.hxx>
 
 #include <svx/svdview.hxx>
 #include <svx/svdpagv.hxx>
@@ -1579,6 +1580,12 @@ void ScGridWindow::PaintTile( VirtualDevice& rDevice,
 
     // draw the content
     DrawContent(rDevice, aTabInfo, aOutputData, true);
+    rDevice.SetMapMode(aOriginalMode);
+
+    // Paint the chart(s) in edit mode.
+    LokChartHelper::PaintAllChartsOnTile(rDevice, nOutputWidth, nOutputHeight,
+        nTilePosX, nTilePosY, nTileWidth, nTileHeight, bLayoutRTL);
+
     rDevice.SetMapMode(aOriginalMode);
 
     // Flag drawn formula cells "unchanged".

cactus-bm/libreoffice-core

Conflicts:
	include/sfx2/lokcharthelper.hxx
	sfx2/source/view/ipclient.cxx
(cherry picked from commit 284068c7eb473bafd5cafeb30a78daab4538cff6)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/129362
Tested-by: Jenkins
Reviewed-by: Dennis Francis <dennis.francis@collabora.com>

sc/source/ui/view/gridwin4.cxx
@@ -35,6 +35,7 @@
 #include <LibreOfficeKit/LibreOfficeKitEnums.h>
 #include <comphelper/lok.hxx>
 #include <sfx2/lokhelper.hxx>
+#include <sfx2/lokcomponenthelpers.hxx>
 
 #include <svx/svdview.hxx>
 #include <svx/svdpagv.hxx>
@@ -1623,6 +1624,12 @@ void ScGridWindow::PaintTile( VirtualDevice& rDevice,
 
     // draw the content
     DrawContent(rDevice, aTabInfo, aOutputData, true);
+    rDevice.SetMapMode(aOriginalMode);
+
+    // Paint the chart(s) in edit mode.
+    LokChartHelper::PaintAllChartsOnTile(rDevice, nOutputWidth, nOutputHeight,
+        nTilePosX, nTilePosY, nTileWidth, nTileHeight, bLayoutRTL);
+
     rDevice.SetMapMode(aOriginalMode);
 
     // Flag drawn formula cells "unchanged".