LibreOffice/core

commit 8611f6e259b807b4f19c8dc0eab86ca648891ce3

ref-count SdrObject
Which means we can get rid of the majestic hack of ScCaptionPtr
Previously, SdrObject was manually managed, and the ownership
passed around in very complicated fashion.

Notes:

(*) SvxShape has a strong reference to SdrObject, where
previously it had a weak reference. It is now strong
since otherwise the SdrObject will go away very eagerly.

(*) SdrObject still has a weak reference to SvxShape

(*) In the existing places that an SdrObject is being
deleted, we now just clear the reference

(*) instead of SwVirtFlyDrawObj removing itself from the
page that contains inside it's destructor, make the call site
do the removing from the page.

(*) Needed to take the SolarMutex in UndoManagerHelper_Impl::impl_clear
because this can be called from UNO (e.g. sfx2_complex JUnit test)
and the SdrObjects need the SolarMutex when destructing.

(*) handle a tricky situation with SwDrawVirtObj in the SwDrawModel
destructor because the existing code wants mpDrawObj in
SwAnchoredObject to be sometimes owning, sometimes not, which
results in a cycle with the new code.

Change-Id: I4d79df1660e386388e5d51030653755bca02a163
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138837
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

reportdesign/inc/RptObject.hxx
@@ -66,7 +66,6 @@ protected:
     mutable rtl::Reference<OPropertyMediator>                         m_xMediator;
     mutable css::uno::Reference< css::beans::XPropertyChangeListener> m_xPropertyChangeListener;
     mutable css::uno::Reference< css::report::XReportComponent>       m_xReportComponent;
-    css::uno::Reference< css::uno::XInterface >                       m_xKeepShapeAlive;
     OUString m_sComponentName;
     bool        m_bIsListening;
 
@@ -83,12 +82,8 @@ protected:
 
     /** called by instances of derived classes to implement their overriding of getUnoShape
     */
-    css::uno::Reference< css::drawing::XShape >
+    static css::uno::Reference< css::drawing::XShape >
             getUnoShapeOf( SdrObject& _rSdrObject );
-
-private:
-    static void    ensureSdrObjectOwnership(
-                    const css::uno::Reference< css::uno::XInterface >& _rxShape );
 
 public:
     OObjectBase(const OObjectBase&) = delete;
@@ -107,11 +102,7 @@ public:
     css::uno::Reference< css::report::XSection> getSection() const;
     const OUString& getServiceName() const { return m_sComponentName; }
 
-    /** releases the reference to our UNO shape (m_xKeepShapeAlive)
-    */
-    void    releaseUnoShape() { m_xKeepShapeAlive.clear(); }
-
-    static SdrObject* createObject(
+    static rtl::Reference<SdrObject> createObject(
         SdrModel& rTargetModel,
         const css::uno::Reference< css::report::XReportComponent>& _xComponent);
     static SdrObjKind getObjectType(const css::uno::Reference< css::report::XReportComponent>& _xComponent);
@@ -129,7 +120,7 @@ private:
     virtual ~OCustomShape() override;
 
 public:
-    static OCustomShape* Create(
+    static rtl::Reference<OCustomShape> Create(
         SdrModel& rSdrModel,
         const css::uno::Reference< css::report::XReportComponent>& _xComponent)
     {
@@ -173,7 +164,7 @@ private:
     virtual ~OOle2Obj() override;
 
 public:
-    static OOle2Obj* Create(
+    static rtl::Reference<OOle2Obj> Create(
         SdrModel& rSdrModel,
         const css::uno::Reference< css::report::XReportComponent>& _xComponent,
         SdrObjKind _nType)
@@ -187,7 +178,7 @@ public:
     virtual SdrObjKind GetObjIdentifier() const override;
     virtual SdrInventor GetObjInventor() const override;
     // Clone() should make a complete copy of the object.
-    virtual OOle2Obj* CloneSdrObject(SdrModel& rTargetModel) const override;
+    virtual rtl::Reference<SdrObject> CloneSdrObject(SdrModel& rTargetModel) const override;
     virtual void initializeOle() override;
 
     void initializeChart( const css::uno::Reference< css::frame::XModel>& _xModel);
@@ -269,10 +260,9 @@ public:
     virtual css::uno::Reference< css::drawing::XShape > getUnoShape() override;
     virtual SdrObjKind GetObjIdentifier() const override;
     virtual SdrInventor GetObjInventor() const override;
-    virtual OUnoObject* CloneSdrObject(SdrModel& rTargetModel) const override;
+    virtual rtl::Reference<SdrObject> CloneSdrObject(SdrModel& rTargetModel) const override;
 
 private:
-    virtual void setUnoShape( const css::uno::Reference< css::drawing::XShape >& rxUnoShape ) override;
     void    impl_initializeModel_nothrow();
 };
 

reportdesign/source/core/sdr/RptObject.cxx
@@ -86,17 +86,17 @@ SdrObjKind OObjectBase::getObjectType(const uno::Reference< report::XReportCompo
     return SdrObjKind::OLE2;
 }
 
-SdrObject* OObjectBase::createObject(
+rtl::Reference<SdrObject> OObjectBase::createObject(
     SdrModel& rTargetModel,
     const uno::Reference< report::XReportComponent>& _xComponent)
 {
-    SdrObject* pNewObj = nullptr;
+    rtl::Reference<SdrObject> pNewObj;
     SdrObjKind nType = OObjectBase::getObjectType(_xComponent);
     switch( nType )
     {
         case SdrObjKind::ReportDesignFixedText:
             {
-                OUnoObject* pUnoObj = new OUnoObject(
+                rtl::Reference<OUnoObject> pUnoObj = new OUnoObject(
                     rTargetModel,
                     _xComponent,
                     OUString("com.sun.star.form.component.FixedText"),
@@ -159,8 +159,6 @@ SdrObject* OObjectBase::createObject(
 
     if ( pNewObj )
         pNewObj->SetDoNotInsertIntoPageAutomatically( true );
-
-    ensureSdrObjectOwnership( _xComponent );
 
     return pNewObj;
 }
@@ -415,27 +413,6 @@ bool OObjectBase::supportsService( const OUString& _sServiceName ) const
 }
 
 
-void OObjectBase::ensureSdrObjectOwnership( const uno::Reference< uno::XInterface >& _rxShape )
-{
-    // UNDO in the report designer is implemented at the level of the XShapes, not
-    // at the level of SdrObjects. That is, if an object is removed from the report
-    // design, then this happens by removing the XShape from the UNO DrawPage, and
-    // putting this XShape (resp. the ReportComponent which wraps it) into an UNDO
-    // action.
-    // Unfortunately, the SvxDrawPage implementation usually deletes SdrObjects
-    // which are removed from it, which is deadly for us. To prevent this,
-    // we give the XShape implementation the ownership of the SdrObject, which
-    // ensures the SvxDrawPage won't delete it.
-    SvxShape* pShape = comphelper::getFromUnoTunnel<SvxShape>( _rxShape );
-    OSL_ENSURE( pShape, "OObjectBase::ensureSdrObjectOwnership: can't access the SvxShape!" );
-    if ( pShape )
-    {
-        OSL_ENSURE( !pShape->HasSdrObjectOwnership(), "OObjectBase::ensureSdrObjectOwnership: called twice?" );
-        pShape->TakeSdrObjectOwnership();
-    }
-}
-
-
 uno::Reference< drawing::XShape > OObjectBase::getUnoShapeOf( SdrObject& _rSdrObject )
 {
     uno::Reference< drawing::XShape > xShape( _rSdrObject.getWeakUnoShape() );
@@ -446,9 +423,6 @@ uno::Reference< drawing::XShape > OObjectBase::getUnoShapeOf( SdrObject& _rSdrOb
     if ( !xShape.is() )
         return xShape;
 
-    ensureSdrObjectOwnership( xShape );
-
-    m_xKeepShapeAlive = xShape;
     return xShape;
 }
 
@@ -566,7 +540,6 @@ uno::Reference< drawing::XShape > OCustomShape::getUnoShape()
 void OCustomShape::setUnoShape( const uno::Reference< drawing::XShape >& rxUnoShape )
 {
     SdrObjCustomShape::setUnoShape( rxUnoShape );
-    releaseUnoShape();
     m_xReportComponent.clear();
 }
 
@@ -894,13 +867,7 @@ uno::Reference< drawing::XShape > OUnoObject::getUnoShape()
     return OObjectBase::getUnoShapeOf( *this );
 }
 
-void OUnoObject::setUnoShape( const uno::Reference< drawing::XShape >& rxUnoShape )
-{
-    SdrUnoObj::setUnoShape( rxUnoShape );
-    releaseUnoShape();
-}
-
-OUnoObject* OUnoObject::CloneSdrObject(SdrModel& rTargetModel) const
+rtl::Reference<SdrObject> OUnoObject::CloneSdrObject(SdrModel& rTargetModel) const
 {
     return new OUnoObject(rTargetModel, *this);
 }
@@ -1093,7 +1060,6 @@ uno::Reference< drawing::XShape > OOle2Obj::getUnoShape()
 void OOle2Obj::setUnoShape( const uno::Reference< drawing::XShape >& rxUnoShape )
 {
     SdrOle2Obj::setUnoShape( rxUnoShape );
-    releaseUnoShape();
     m_xReportComponent.clear();
 }
 
@@ -1114,7 +1080,7 @@ static uno::Reference< chart2::data::XDatabaseDataProvider > lcl_getDataProvider
 }
 
 // Clone() should make a complete copy of the object.
-OOle2Obj* OOle2Obj::CloneSdrObject(SdrModel& rTargetModel) const
+rtl::Reference<SdrObject> OOle2Obj::CloneSdrObject(SdrModel& rTargetModel) const
 {
     return new OOle2Obj(rTargetModel, *this);
 }

reportdesign/source/core/sdr/RptPage.cxx
@@ -82,9 +82,9 @@ void OReportPage::removeSdrObject(const uno::Reference< report::XReportComponent
     }
 }
 
-SdrObject* OReportPage::RemoveObject(size_t nObjNum)
+rtl::Reference<SdrObject> OReportPage::RemoveObject(size_t nObjNum)
 {
-    SdrObject* pObj = SdrPage::RemoveObject(nObjNum);
+    rtl::Reference<SdrObject> pObj = SdrPage::RemoveObject(nObjNum);
     if (getSpecialMode())
     {
         return pObj;
@@ -94,7 +94,7 @@ SdrObject* OReportPage::RemoveObject(size_t nObjNum)
     reportdesign::OSection* pSection = comphelper::getFromUnoTunnel<reportdesign::OSection>(m_xSection);
     uno::Reference< drawing::XShape> xShape(pObj->getUnoShape(),uno::UNO_QUERY);
     pSection->notifyElementRemoved(xShape);
-    if (dynamic_cast< const OUnoObject *>( pObj ) !=  nullptr)
+    if (dynamic_cast< const OUnoObject *>( pObj.get() ) !=  nullptr)
     {
         OUnoObject& rUnoObj = dynamic_cast<OUnoObject&>(*pObj);
         uno::Reference< container::XChild> xChild(rUnoObj.GetUnoControlModel(),uno::UNO_QUERY);
@@ -178,13 +178,6 @@ void OReportPage::NbcInsertObject(SdrObject* pObj, size_t nPos)
     reportdesign::OSection* pSection = comphelper::getFromUnoTunnel<reportdesign::OSection>(m_xSection);
     uno::Reference< drawing::XShape> xShape(pObj->getUnoShape(),uno::UNO_QUERY);
     pSection->notifyElementAdded(xShape);
-
-    // now that the shape is inserted into its structures, we can allow the OObjectBase
-    // to release the reference to it
-    OObjectBase* pObjectBase = dynamic_cast< OObjectBase* >( pObj );
-    OSL_ENSURE( pObjectBase, "OReportPage::NbcInsertObject: what is being inserted here?" );
-    if ( pObjectBase )
-        pObjectBase->releaseUnoShape();
 }
 
 } // rptui

cactus-bm/libreoffice-core

commit 250668474b660b8007670c4bcfb2dabc3bc99ceb

tdf#150732 ReportBuilder: Moving fields from one section to another crashes
regression from
        commit 8611f6e259b807b4f19c8dc0eab86ca648891ce3
        Author: Noel Grandin <noel.grandin@collabora.co.uk>
        Date:   Thu May 27 10:27:46 2021 +0200
        ref-count SdrObject

Fixes 2 issues
(1) where I removed some code that we need
(2) where the OUnoObject constructor was deleting the object is was
constructing

Note that this only fixes the crash that Julien saw, not the underlying
problem that this tdf bug reports. That bug appears to predate my
commit.

Change-Id: I3878ef460dedc3c2a6c86b88fce9d155e79bc0b8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/139714
Tested-by: Julien Nabet <serval2412@yahoo.fr>
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

reportdesign/inc/RptObject.hxx
@@ -66,6 +66,7 @@ protected:
     mutable rtl::Reference<OPropertyMediator>                         m_xMediator;
     mutable css::uno::Reference< css::beans::XPropertyChangeListener> m_xPropertyChangeListener;
     mutable css::uno::Reference< css::report::XReportComponent>       m_xReportComponent;
+    css::uno::Reference< css::uno::XInterface >                       m_xKeepShapeAlive;
     OUString m_sComponentName;
     bool        m_bIsListening;
 
@@ -82,7 +83,7 @@ protected:
 
     /** called by instances of derived classes to implement their overriding of getUnoShape
     */
-    static css::uno::Reference< css::drawing::XShape >
+    css::uno::Reference< css::drawing::XShape >
             getUnoShapeOf( SdrObject& _rSdrObject );
 
 public:
@@ -101,6 +102,10 @@ public:
     virtual css::uno::Reference< css::beans::XPropertySet> getAwtComponent();
     css::uno::Reference< css::report::XSection> getSection() const;
     const OUString& getServiceName() const { return m_sComponentName; }
+
+    /** releases the reference to our UNO shape (m_xKeepShapeAlive)
+    */
+    void    releaseUnoShape() { m_xKeepShapeAlive.clear(); }
 
     static rtl::Reference<SdrObject> createObject(
         SdrModel& rTargetModel,
@@ -263,6 +268,7 @@ public:
     virtual rtl::Reference<SdrObject> CloneSdrObject(SdrModel& rTargetModel) const override;
 
 private:
+    virtual void setUnoShape( const css::uno::Reference< css::drawing::XShape >& rxUnoShape ) override;
     void    impl_initializeModel_nothrow();
 };
 

reportdesign/source/core/sdr/RptObject.cxx
@@ -423,6 +423,7 @@ uno::Reference< drawing::XShape > OObjectBase::getUnoShapeOf( SdrObject& _rSdrOb
     if ( !xShape.is() )
         return xShape;
 
+    m_xKeepShapeAlive = xShape;
     return xShape;
 }
 
@@ -540,6 +541,7 @@ uno::Reference< drawing::XShape > OCustomShape::getUnoShape()
 void OCustomShape::setUnoShape( const uno::Reference< drawing::XShape >& rxUnoShape )
 {
     SdrObjCustomShape::setUnoShape( rxUnoShape );
+    releaseUnoShape();
     m_xReportComponent.clear();
 }
 
@@ -566,12 +568,16 @@ OUnoObject::OUnoObject(
     // tdf#119067
     ,m_bSetDefaultLabel(rSource.m_bSetDefaultLabel)
 {
-    if ( !rSource.getUnoControlModelTypeName().isEmpty() )
-        impl_initializeModel_nothrow();
-    Reference<XPropertySet> xSource(const_cast<OUnoObject&>(rSource).getUnoShape(), uno::UNO_QUERY);
-    Reference<XPropertySet> xDest(getUnoShape(), uno::UNO_QUERY);
-    if ( xSource.is() && xDest.is() )
-        comphelper::copyProperties(xSource, xDest);
+    osl_atomic_increment(&m_refCount); // getUnoShape will ref-count thiss
+    {
+        if ( !rSource.getUnoControlModelTypeName().isEmpty() )
+            impl_initializeModel_nothrow();
+        Reference<XPropertySet> xSource(const_cast<OUnoObject&>(rSource).getUnoShape(), uno::UNO_QUERY);
+        Reference<XPropertySet> xDest(getUnoShape(), uno::UNO_QUERY);
+        if ( xSource.is() && xDest.is() )
+            comphelper::copyProperties(xSource, xDest);
+    }
+    osl_atomic_decrement(&m_refCount);
 }
 
 OUnoObject::OUnoObject(
@@ -867,6 +873,12 @@ uno::Reference< drawing::XShape > OUnoObject::getUnoShape()
     return OObjectBase::getUnoShapeOf( *this );
 }
 
+void OUnoObject::setUnoShape( const uno::Reference< drawing::XShape >& rxUnoShape )
+{
+    SdrUnoObj::setUnoShape( rxUnoShape );
+    releaseUnoShape();
+}
+
 rtl::Reference<SdrObject> OUnoObject::CloneSdrObject(SdrModel& rTargetModel) const
 {
     return new OUnoObject(rTargetModel, *this);
@@ -1060,6 +1072,7 @@ uno::Reference< drawing::XShape > OOle2Obj::getUnoShape()
 void OOle2Obj::setUnoShape( const uno::Reference< drawing::XShape >& rxUnoShape )
 {
     SdrOle2Obj::setUnoShape( rxUnoShape );
+    releaseUnoShape();
     m_xReportComponent.clear();
 }
 

reportdesign/source/core/sdr/RptPage.cxx
@@ -178,6 +178,13 @@ void OReportPage::NbcInsertObject(SdrObject* pObj, size_t nPos)
     reportdesign::OSection* pSection = comphelper::getFromUnoTunnel<reportdesign::OSection>(m_xSection);
     uno::Reference< drawing::XShape> xShape(pObj->getUnoShape(),uno::UNO_QUERY);
     pSection->notifyElementAdded(xShape);
+
+    // now that the shape is inserted into its structures, we can allow the OObjectBase
+    // to release the reference to it
+    OObjectBase* pObjectBase = dynamic_cast< OObjectBase* >( pObj );
+    OSL_ENSURE( pObjectBase, "OReportPage::NbcInsertObject: what is being inserted here?" );
+    if ( pObjectBase )
+        pObjectBase->releaseUnoShape();
 }
 
 } // rptui

svx/source/unodraw/unoprov.cxx
@@ -838,7 +838,7 @@ OUString UHashMap::getNameFromId(SdrObjKind nId)
         [nId](const UHashMapImpl::value_type& rEntry) { return rEntry.second == nId; });
     if (it != rMap.end())
         return it->first;
-    OSL_FAIL("[CL] unknown SdrObject identifier");
+    SAL_WARN("svx", "[CL] unknown SdrObjKind identifier " << static_cast<int>(nId));
     return OUString();
 }
 