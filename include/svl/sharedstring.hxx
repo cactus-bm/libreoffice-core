LibreOffice/core

commit b52fd544b9c128d3e28de6355286f6480b618e93

avoid some string refcounting with sheets with lots of conditional formatting
Change-Id: Ibb2de1aa28a57748bede6085074f60af757ae5ef
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/140244
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/core/tool/interpr1.cxx
@@ -8178,7 +8178,8 @@ void ScInterpreter::ScIndirect()
         bTryXlA1 = false;
     }
 
-    OUString sRefStr = GetString().getString();
+    svl::SharedString sSharedRefStr = GetString();
+    const OUString & sRefStr = sSharedRefStr.getString();
     if (sRefStr.isEmpty())
     {
         // Bail out early for empty cells, rely on "we do have a string" below.
@@ -8223,7 +8224,7 @@ void ScInterpreter::ScIndirect()
 
     do
     {
-        OUString aName( ScGlobal::getCharClass().uppercase( sRefStr));
+        const OUString & aName( sSharedRefStr.getDataIgnoreCase() );
         ScDBCollection::NamedDBs& rDBs = mrDoc.GetDBCollection()->getNamedDBs();
         const ScDBData* pData = rDBs.findByUpperName( aName);
         if (!pData)

sc/source/core/tool/cellform.cxx
@@ -146,12 +146,15 @@ OUString ScCellFormat::GetInputString(
         break;
         case CELLTYPE_FORMULA:
         {
-            OUString str;
+            std::optional<OUString> str;
             ScFormulaCell* pFC = rCell.getFormula();
             if (pFC->IsEmptyDisplayedAsString())
                 ; // empty
             else if (pFC->IsValue())
-                rFormatter.GetInputLineString(pFC->GetValue(), nFormat, str, bFiltering, bForceSystemLocale);
+            {
+                str.emplace();
+                rFormatter.GetInputLineString(pFC->GetValue(), nFormat, *str, bFiltering, bForceSystemLocale);
+            }
             else
             {
                 const svl::SharedString& shared = pFC->GetString();
@@ -166,19 +169,19 @@ OUString ScCellFormat::GetInputString(
             const FormulaError nErrCode = pFC->GetErrCode();
             if (nErrCode != FormulaError::NONE)
             {
-                str.clear();
+                str.reset();
                 if( pShared != nullptr )
                     *pShared = nullptr;
             }
 
-            return str;
+            return str ? std::move(*str) : svl::SharedString::EMPTY_STRING;
         }
         case CELLTYPE_NONE:
             if( pShared != nullptr )
                 *pShared = &svl::SharedString::getEmptyString();
-            return OUString();
+            return svl::SharedString::EMPTY_STRING;
         default:
-            return OUString();
+            return svl::SharedString::EMPTY_STRING;
     }
 }
 

cactus-bm/libreoffice-core

commit 9bde0c868ee4b1729fa33b509450a36627cb0332

crashtesting: forum-de2-1635.ods
regression from
    commit b52fd544b9c128d3e28de6355286f6480b618e93
    Date:   Tue Sep 20 15:48:38 2022 +0200
    avoid some string refcounting with sheets with lots of conditional
formatting

Change-Id: If1414221aef0b227200cfb6ffb8e87abb48a90c0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/140589
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/svl/sharedstring.hxx
@@ -38,6 +38,7 @@ public:
     bool operator!= ( const SharedString& r ) const;
 
     const OUString & getString() const;
+    const OUString & getIgnoreCaseString() const;
 
     rtl_uString* getData();
     const rtl_uString* getData() const;
@@ -115,6 +116,11 @@ inline bool SharedString::operator!= ( const SharedString& r ) const
 inline const OUString & SharedString::getString() const
 {
     return mpData ? OUString::unacquired(&mpData) : EMPTY_STRING;
+}
+
+inline const OUString & SharedString::getIgnoreCaseString() const
+{
+    return mpDataIgnoreCase ? OUString::unacquired(&mpDataIgnoreCase) : EMPTY_STRING;
 }
 
 inline rtl_uString* SharedString::getData()

sc/source/core/tool/interpr1.cxx
@@ -8224,7 +8224,7 @@ void ScInterpreter::ScIndirect()
 
     do
     {
-        const OUString & aName( sSharedRefStr.getDataIgnoreCase() );
+        const OUString & aName( sSharedRefStr.getIgnoreCaseString() );
         ScDBCollection::NamedDBs& rDBs = mrDoc.GetDBCollection()->getNamedDBs();
         const ScDBData* pData = rDBs.findByUpperName( aName);
         if (!pData)