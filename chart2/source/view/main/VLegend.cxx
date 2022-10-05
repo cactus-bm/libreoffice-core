LibreOffice/core

commit 258a8d133d0548c7af51f4852260e9969df288fc

tdf#150034 very slow opening pathological chart
This chart has a ridiculously long legend, which makes the loop that
tries to shorten legends take an extremely long time.

Make this loop faster, at the risk of sometimes getting a slightly wrong
answer.
Ideally we could speed up the layout down in ImpEditEngine, but that is
a much bigger task.

Change-Id: I7cb337c674515bac13e4b11c3b0fabb94aed6865
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137677
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

chart2/source/view/main/VLegend.cxx
@@ -418,7 +418,7 @@ awt::Size lcl_placeLegendEntries(
                     {
                         OUString aLabelString = rEntries[0].aLabel[0]->getString();
                         static const OUStringLiteral sDots = u"...";
-                        for (sal_Int32 nNewLen = aLabelString.getLength() - sDots.getLength(); nNewLen > 0; nNewLen--)
+                        for (sal_Int32 nNewLen = aLabelString.getLength() - sDots.getLength(); nNewLen > 0; )
                         {
                             OUString aNewLabel = aLabelString.subView(0, nNewLen) + sDots;
                             rtl::Reference<SvxShapeText> xEntry = ShapeFactory::createText(
@@ -438,6 +438,12 @@ awt::Size lcl_placeLegendEntries(
                                 }
                             }
                             DrawModelWrapper::removeShape(xEntry);
+                            // The intention here is to make pathological cases with extremely large labels
+                            // converge a little faster
+                            if (std::abs(nRemainingSpace) > nSumHeight / 10)
+                                nNewLen -= nNewLen / 10;
+                            else
+                                --nNewLen;
                         }
                         if (aTextShapes.size() == 0)
                         {

cactus-bm/libreoffice-core

commit 7ff0db357602689a4a05267e2e00ada380fe7d2b

tdf#151283 calc hangs when try to open XLS file
regression from
    commit 258a8d133d0548c7af51f4852260e9969df288fc
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    tdf#150034 very slow opening pathological chart

if nNewLen is < 10, we can get stuck in an infinite loop
because the calculation will not actually decrement nNewLen

Change-Id: I98920deaa087713c44531a7f6506d1d5d32b2e98
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/140949
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

chart2/source/view/main/VLegend.cxx
@@ -448,7 +448,7 @@ awt::Size lcl_placeLegendEntries(
                             DrawModelWrapper::removeShape(xEntry);
                             // The intention here is to make pathological cases with extremely large labels
                             // converge a little faster
-                            if (std::abs(nRemainingSpace) > nSumHeight / 10)
+                            if (nNewLen > 10 && std::abs(nRemainingSpace) > nSumHeight / 10)
                                 nNewLen -= nNewLen / 10;
                             else
                                 --nNewLen;