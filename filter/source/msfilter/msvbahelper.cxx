LibreOffice/core

commit beb6c62e990599d91ac5d9183164c94d269027d3

vba: fix registering shortcuts keys defined by the vba macros
On issue with registering was that the registering happened when
the macro source was in	the process to be read into the library,
which is just a	bit too	early, because the macro wasn't	found and
not registered.
Another	issue was with searching for the macro method (hasMacro),
which doesn't search the same when the module name is known and
when it isn't. This was	changed	so we just iterate through the
modules	and call the same "FindMethod" method without any extra
restrictions.

Change-Id: I811cfcaca58e8dfa8bef6cf983a8aff2b60eba35
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/129196
Tested-by: Jenkins
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

filter/source/msfilter/msvbahelper.cxx
@@ -173,8 +173,6 @@ static SfxObjectShell* findShellForUrl( const OUString& sMacroURLOrPath )
 // if sMod is empty, only standard modules will be searched (no class, document, form modules)
 static bool hasMacro( SfxObjectShell const * pShell, const OUString& sLibrary, OUString& sMod, const OUString& sMacro )
 {
-    bool bFound = false;
-
 #if !HAVE_FEATURE_SCRIPTING
     (void) pShell;
     (void) sLibrary;
@@ -200,18 +198,17 @@ static bool hasMacro( SfxObjectShell const * pShell, const OUString& sLibrary, O
                     SbModule* pModule = pBasic->FindModule( sMod );
                     if ( pModule && pModule->FindMethod( sMacro, SbxClassType::Method ))
                     {
-                        bFound = true;
+                        return true;
                     }
                 }
-                else if( SbMethod* pMethod = dynamic_cast< SbMethod* >( pBasic->Find( sMacro, SbxClassType::Method ) ) )
+                else
                 {
-                    if( SbModule* pModule = pMethod->GetModule() )
+                    for (auto const& rModuleRef : pBasic->GetModules())
                     {
-                        // when searching for a macro without module name, do not search in class/document/form modules
-                        if( pModule->GetModuleType() == script::ModuleType::NORMAL )
+                        if (rModuleRef && rModuleRef->FindMethod(sMacro, SbxClassType::Method))
                         {
-                            sMod = pModule->GetName();
-                            bFound = true;
+                            sMod = rModuleRef->GetName();
+                            return true;
                         }
                     }
                 }
@@ -219,7 +216,7 @@ static bool hasMacro( SfxObjectShell const * pShell, const OUString& sLibrary, O
         }
     }
 #endif
-    return bFound;
+    return false;
 }
 
 #endif

include/oox/ole/vbamodule.hxx
@@ -24,6 +24,7 @@
 #include <rtl/textenc.h>
 #include <rtl/ustring.hxx>
 #include <sal/types.h>
+#include <vector>
 
 namespace com::sun::star {
     namespace container { class XNameAccess; }
@@ -39,6 +40,11 @@ namespace oox {
 
 namespace oox::ole {
 
+struct VbaKeyBinding
+{
+    OUString msApiKey;
+    OUString msMethodName;
+};
 
 class VbaModule
 {
@@ -67,15 +73,17 @@ public:
     void                createAndImportModule(
                             StorageBase& rVbaStrg,
                             const css::uno::Reference< css::container::XNameContainer >& rxBasicLib,
-                            const css::uno::Reference< css::container::XNameAccess >& rxDocObjectNA ) const;
+                            const css::uno::Reference< css::container::XNameAccess >& rxDocObjectNA );
     /** Creates an empty Basic module in the passed Basic library. */
     void                createEmptyModule(
                             const css::uno::Reference< css::container::XNameContainer >& rxBasicLib,
                             const css::uno::Reference< css::container::XNameAccess >& rxDocObjectNA ) const;
 
+    void registerShortcutKeys();
+
 private:
     /** Reads and returns the VBA source code from the passed storage. */
-    OUString     readSourceCode( StorageBase& rVbaStrg ) const;
+    OUString     readSourceCode( StorageBase& rVbaStrg );
 
     /** Creates a new Basic module and inserts it into the passed Basic library. */
     void                createModule(
@@ -97,6 +105,7 @@ private:
     bool                mbReadOnly;
     bool                mbPrivate;
     bool                mbExecutable;
+    std::vector<VbaKeyBinding> maKeyBindings;
 };
 
 

oox/source/ole/vbamodule.cxx
@@ -127,10 +127,26 @@ void VbaModule::importDirRecords( BinaryInputStream& rDirStrm )
 
 void VbaModule::createAndImportModule( StorageBase& rVbaStrg,
                                        const Reference< container::XNameContainer >& rxBasicLib,
-                                       const Reference< container::XNameAccess >& rxDocObjectNA ) const
+                                       const Reference< container::XNameAccess >& rxDocObjectNA )
 {
     OUString aVBASourceCode = readSourceCode( rVbaStrg );
     createModule( aVBASourceCode, rxBasicLib, rxDocObjectNA );
+    registerShortcutKeys();
+}
+
+void VbaModule::registerShortcutKeys()
+{
+    for (VbaKeyBinding const& rKeyBinding : maKeyBindings)
+    {
+        try
+        {
+            KeyEvent aKeyEvent = ooo::vba::parseKeyEvent(rKeyBinding.msApiKey);
+            ooo::vba::applyShortCutKeyBinding(mxDocModel, aKeyEvent, rKeyBinding.msMethodName);
+        }
+        catch (const Exception&)
+        {
+        }
+    }
 }
 
 void VbaModule::createEmptyModule( const Reference< container::XNameContainer >& rxBasicLib,
@@ -139,7 +155,7 @@ void VbaModule::createEmptyModule( const Reference< container::XNameContainer >&
     createModule( u"", rxBasicLib, rxDocObjectNA );
 }
 
-OUString VbaModule::readSourceCode( StorageBase& rVbaStrg ) const
+OUString VbaModule::readSourceCode( StorageBase& rVbaStrg )
 {
     OUStringBuffer aSourceCode(512);
     static const char sUnmatchedRemovedTag[] = "Rem removed unmatched Sub/End: ";
@@ -189,14 +205,7 @@ OUString VbaModule::readSourceCode( StorageBase& rVbaStrg ) const
                             // cntrl modifier is explicit ( but could be cntrl+shift ), parseKeyEvent
                             // will handle and uppercase letter appropriately
                             OUString sApiKey = "^" + sKey;
-                            try
-                            {
-                                KeyEvent aKeyEvent = ooo::vba::parseKeyEvent( sApiKey );
-                                ooo::vba::applyShortCutKeyBinding( mxDocModel, aKeyEvent, sProc );
-                            }
-                            catch (const Exception&)
-                            {
-                            }
+                            maKeyBindings.push_back({sApiKey, sProc});
                         }
                     }
                 }

cactus-bm/libreoffice-core

commit 09444cda904b0f7ae901b20753be4717c1a8c22f

tdf#148806 tdf#151393 xls vba: no Auto_Open from ThisWorksheet
Unlike Word, ThisWorksheet cannot hold auto-running
subroutines for Open/Close/New.

This fixes a LO 7.4 regression caused by
commit beb6c62e990599d91ac5d9183164c94d269027d3.

Change-Id: Idb8f72775d9392b306cb924ee776821272b12f3b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/141127
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

filter/source/msfilter/msvbahelper.cxx
@@ -172,7 +172,7 @@ static SfxObjectShell* findShellForUrl( const OUString& sMacroURLOrPath )
 // if sMod is empty and a macro is found then sMod is updated
 // if sMod is empty, only standard modules will be searched (no class, document, form modules)
 static bool hasMacro(SfxObjectShell const* pShell, const OUString& sLibrary, OUString& sMod,
-                     const OUString& sMacro, bool bOnlyPublic)
+                     const OUString& sMacro, bool bOnlyPublic, const OUString& sSkipModule)
 {
 #if !HAVE_FEATURE_SCRIPTING
     (void) pShell;
@@ -180,6 +180,7 @@ static bool hasMacro(SfxObjectShell const* pShell, const OUString& sLibrary, OUS
     (void) sMod;
     (void) sMacro;
     (void) bOnlyPublic;
+    (void) sSkipModule;
 #else
     if (sLibrary.isEmpty() || sMacro.isEmpty())
         return false;
@@ -212,7 +213,8 @@ static bool hasMacro(SfxObjectShell const* pShell, const OUString& sLibrary, OUS
         SbMethod* pMeth = rModuleRef->FindMethod(sMacro, SbxClassType::Method);
         if (pMeth)
         {
-            if (bOnlyPublic && pMeth->IsSet(SbxFlagBits::Private))
+            if ((bOnlyPublic && pMeth->IsSet(SbxFlagBits::Private))
+                || rModuleRef->GetName() == sSkipModule)
                 continue;
             sMod = rModuleRef->GetName();
             return true;
@@ -263,7 +265,7 @@ static void parseMacro( const OUString& sMacro, OUString& sContainer, OUString& 
 
 OUString resolveVBAMacro(SfxObjectShell const* pShell, const OUString& rLibName,
                          const OUString& rModuleName, const OUString& rMacroName,
-                         bool bOnlyPublic)
+                         bool bOnlyPublic, const OUString& sSkipModule)
 {
 #if !HAVE_FEATURE_SCRIPTING
     (void) pShell;
@@ -271,12 +273,13 @@ OUString resolveVBAMacro(SfxObjectShell const* pShell, const OUString& rLibName,
     (void) rModuleName;
     (void) rMacroName;
     (void) bOnlyPublic;
+    (void) sSkipModule;
 #else
     if( pShell )
     {
         OUString aLibName = rLibName.isEmpty() ?  getDefaultProjectName( pShell ) : rLibName ;
         OUString aModuleName = rModuleName;
-        if (hasMacro( pShell, aLibName, aModuleName, rMacroName, bOnlyPublic))
+        if (hasMacro(pShell, aLibName, aModuleName, rMacroName, bOnlyPublic, sSkipModule))
             return aLibName + "." + aModuleName + "." + rMacroName;
     }
 #endif
@@ -447,7 +450,7 @@ MacroResolvedInfo resolveVBAMacro( SfxObjectShell* pShell, const OUString& Macro
 
     for (auto const& search : sSearchList)
     {
-        aRes.mbFound = hasMacro(pShell, search, sModule, sProcedure, /*bOnlyPublic=*/false);
+        aRes.mbFound = hasMacro(pShell, search, sModule, sProcedure, /*bOnlyPublic=*/false, "");
         if ( aRes.mbFound )
         {
             sContainer = search;

include/filter/msfilter/msvbahelper.hxx
@@ -60,7 +60,8 @@ MSFILTER_DLLPUBLIC OUString extractMacroName( std::u16string_view rMacroUrl );
 MSFILTER_DLLPUBLIC OUString getDefaultProjectName( SfxObjectShell const * pShell );
 MSFILTER_DLLPUBLIC OUString resolveVBAMacro(SfxObjectShell const* pShell, const OUString& rLibName,
                                             const OUString& rModuleName,
-                                            const OUString& rMacroName, bool bOnlyPublic);
+                                            const OUString& rMacroName, bool bOnlyPublic,
+                                            const OUString& sSkipModule);
 MSFILTER_DLLPUBLIC MacroResolvedInfo resolveVBAMacro( SfxObjectShell* pShell, const OUString& rMacroName, bool bSearchGlobalTemplates = false );
 MSFILTER_DLLPUBLIC bool executeMacro( SfxObjectShell* pShell, const OUString& sMacroName, css::uno::Sequence< css::uno::Any >& aArgs, css::uno::Any& aRet, const css::uno::Any& aCaller );
 /// @throws css::uno::RuntimeException

vbahelper/source/vbahelper/vbaeventshelperbase.cxx
@@ -364,11 +364,32 @@ VbaEventsHelperBase::ModulePathMap& VbaEventsHelperBase::updateModulePathMap( co
     sal_Int32 nModuleType = getModuleType( rModuleName );
     // search for all event handlers
     ModulePathMap& rPathMap = maEventPaths[ rModuleName ];
+
+    // Use WORKBOOK_OPEN as a way to get the codename for ThisWorkbook
+    OUString sThisWorkbook;
+    if (getImplementationName() == "ScVbaEventsHelper")
+    {
+        EventHandlerInfo& rThisWorksheetInfo
+            = maEventInfos[css::script::vba::VBAEventId::WORKBOOK_OPEN];
+        css::uno::Sequence<css::uno::Any> aNoArgs;
+        sThisWorkbook = implGetDocumentModuleName(rThisWorksheetInfo, aNoArgs);
+    }
+
     for( const auto& rEventInfo : maEventInfos )
     {
         const EventHandlerInfo& rInfo = rEventInfo.second;
         if( rInfo.mnModuleType == nModuleType )
         {
+            OUString sSkipModule;
+            // Only in Calc, ignore Auto_* in ThisWorkbook
+            if (getImplementationName() == "ScVbaEventsHelper"
+                && (rInfo.mnEventId == css::script::vba::VBAEventId::AUTO_NEW
+                    || rInfo.mnEventId == css::script::vba::VBAEventId::AUTO_OPEN
+                    || rInfo.mnEventId == css::script::vba::VBAEventId::AUTO_CLOSE))
+            {
+                sSkipModule = sThisWorkbook;
+            }
+
             // Only in Word, Auto* only runs if defined as Public, not Private.
             const bool bOnlyPublic
                 = getImplementationName() == "SwVbaEventsHelper"
@@ -380,7 +401,7 @@ VbaEventsHelperBase::ModulePathMap& VbaEventsHelperBase::updateModulePathMap( co
                       || rInfo.mnEventId == css::script::vba::VBAEventId::AUTO_CLOSE);
 
             OUString sName = resolveVBAMacro(mpShell, maLibraryName, rModuleName,
-                                             rInfo.maMacroName, bOnlyPublic);
+                                             rInfo.maMacroName, bOnlyPublic, sSkipModule);
             // Only in Word (with lowest priority), an Auto* module can execute a "Public Sub Main"
             if (sName.isEmpty() && rModuleName.isEmpty()
                 && getImplementationName() == "SwVbaEventsHelper")
@@ -390,7 +411,7 @@ VbaEventsHelperBase::ModulePathMap& VbaEventsHelperBase::updateModulePathMap( co
                     || rInfo.mnEventId == css::script::vba::VBAEventId::AUTO_CLOSE)
                 {
                     sName = resolveVBAMacro(mpShell, maLibraryName, rInfo.maMacroName, "Main",
-                                            bOnlyPublic);
+                                            bOnlyPublic, sSkipModule);
                 }
             }
             rPathMap[rInfo.mnEventId] = sName;