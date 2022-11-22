LibreOffice/core

commit 6cb400f41df0dd108cdb4b4d3ec6656844814147

store the SfxItemSet inside SfxSetItem by value
rather than on the heap, avoiding an allocation

Change-Id: I3f1504f9a2d4178a9ba59e98de182a0ab98cdce0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/118356
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svl/source/items/sitem.cxx
@@ -29,23 +29,25 @@
 
 SfxSetItem::SfxSetItem( sal_uInt16 which, const SfxItemSet &rSet) :
     SfxPoolItem(which),
-    pSet(rSet.Clone())
+    maSet(rSet)
 {
+    assert(!dynamic_cast<const SfxAllItemSet*>(&rSet) && "cannot handle SfxAllItemSet here");
 }
 
 
-SfxSetItem::SfxSetItem( sal_uInt16 which, std::unique_ptr<SfxItemSet> &&pS) :
+SfxSetItem::SfxSetItem( sal_uInt16 which, SfxItemSet &&pS) :
     SfxPoolItem(which),
-    pSet(std::move(pS))
+    maSet(pS)
 {
-    DBG_ASSERT(pSet, "SfxSetItem without set constructed" );
+    assert(!dynamic_cast<SfxAllItemSet*>(&pS) && "cannot handle SfxAllItemSet here");
 }
 
 
 SfxSetItem::SfxSetItem( const SfxSetItem& rCopy, SfxItemPool *pPool ) :
     SfxPoolItem(rCopy),
-    pSet(rCopy.pSet->Clone(true, pPool))
+    maSet(rCopy.maSet.CloneAsValue(true, pPool))
 {
+    assert(!dynamic_cast<const SfxAllItemSet*>(&rCopy) && "cannot handle SfxAllItemSet here");
 }
 
 
@@ -57,7 +59,7 @@ SfxSetItem::~SfxSetItem()
 bool SfxSetItem::operator==( const SfxPoolItem& rCmp) const
 {
     assert(SfxPoolItem::operator==(rCmp));
-    return *pSet == *static_cast<const SfxSetItem &>(rCmp).pSet;
+    return maSet == static_cast<const SfxSetItem &>(rCmp).maSet;
 }
 
 

compilerplugins/clang/redundantfcast.cxx
@@ -313,6 +313,9 @@ public:
         // compile-time check of constant
         if (fn == SRCDIR "/bridges/source/jni_uno/jni_bridge.cxx")
             return false;
+        // TODO constructing a temporary to pass to a && param
+        if (fn == SRCDIR "/sc/source/ui/view/viewfunc.cxx")
+            return false;
         return true;
     }
 

editeng/source/items/textitem.cxx
@@ -2405,7 +2405,7 @@ bool SvxCharReliefItem::QueryValue( css::uno::Any& rVal,
 *************************************************************************/
 
 SvxScriptSetItem::SvxScriptSetItem( sal_uInt16 nSlotId, SfxItemPool& rPool )
-    : SfxSetItem( nSlotId, std::make_unique<SfxItemSet>( rPool,
+    : SfxSetItem( nSlotId, SfxItemSet( rPool,
                         svl::Items<SID_ATTR_CHAR_FONT, SID_ATTR_CHAR_FONT>{} ))
 {
     sal_uInt16 nLatin, nAsian, nComplex;

include/svl/setitem.hxx
@@ -21,17 +21,18 @@
 
 #include <sal/config.h>
 
+#include <optional>
 #include <svl/poolitem.hxx>
-#include <memory>
+#include <svl/itemset.hxx>
 
 class SVL_DLLPUBLIC SfxSetItem : public SfxPoolItem
 {
-    std::unique_ptr<SfxItemSet> pSet;
+    SfxItemSet maSet;
 
     SfxSetItem& operator=(const SfxSetItem&) = delete;
 
 public:
-    SfxSetItem(sal_uInt16 nWhich, std::unique_ptr<SfxItemSet>&& pSet);
+    SfxSetItem(sal_uInt16 nWhich, SfxItemSet&& pSet);
     SfxSetItem(sal_uInt16 nWhich, const SfxItemSet& rSet);
     SfxSetItem(const SfxSetItem&, SfxItemPool* pPool = nullptr);
     virtual ~SfxSetItem() override;
@@ -45,8 +46,8 @@ public:
     // create a copy of itself
     virtual SfxSetItem* Clone(SfxItemPool* pPool = nullptr) const override = 0;
 
-    const SfxItemSet& GetItemSet() const { return *pSet; }
-    SfxItemSet& GetItemSet() { return *pSet; }
+    const SfxItemSet& GetItemSet() const { return maSet; }
+    SfxItemSet& GetItemSet() { return maSet; }
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

include/svx/pageitem.hxx
@@ -101,7 +101,7 @@ class SVX_DLLPUBLIC SvxSetItem final : public SfxSetItem
 public:
     SvxSetItem( const sal_uInt16 nId, const SfxItemSet& rSet );
     SvxSetItem( const SvxSetItem& rItem );
-    SvxSetItem( const sal_uInt16 nId, std::unique_ptr<SfxItemSet>&& pSet );
+    SvxSetItem( const sal_uInt16 nId, SfxItemSet&& pSet );
 
     virtual SvxSetItem* Clone( SfxItemPool *pPool = nullptr ) const override;
 

include/svx/xflasit.hxx
@@ -31,7 +31,7 @@
 class SVXCORE_DLLPUBLIC XFillAttrSetItem final : public SfxSetItem
 {
 public:
-                            XFillAttrSetItem(std::unique_ptr<SfxItemSet>&& pItemSet );
+                            XFillAttrSetItem(SfxItemSet&& pItemSet );
                             XFillAttrSetItem(SfxItemPool* pItemPool);
                             XFillAttrSetItem(const XFillAttrSetItem& rAttr);
                             XFillAttrSetItem(const XFillAttrSetItem& rAttr,

include/svx/xlnasit.hxx
@@ -31,7 +31,7 @@
 class SVXCORE_DLLPUBLIC XLineAttrSetItem final : public SfxSetItem
 {
 public:
-                            XLineAttrSetItem(std::unique_ptr<SfxItemSet>&& pItemSet );
+                            XLineAttrSetItem(SfxItemSet&& pItemSet );
                             XLineAttrSetItem(SfxItemPool* pItemPool);
                             XLineAttrSetItem(const XLineAttrSetItem& rAttr);
                             XLineAttrSetItem(const XLineAttrSetItem& rAttr,

sc/inc/attarray.hxx
@@ -25,6 +25,7 @@
 
 #include <algorithm>
 #include <memory>
+#include <optional>
 
 #include <svl/itemset.hxx>
 
@@ -64,7 +65,7 @@ struct ScLineFlags
 
 struct ScMergePatternState
 {
-    std::unique_ptr<SfxItemSet> pItemSet;
+    std::optional<SfxItemSet> pItemSet;
     const ScPatternAttr* pOld1;     ///< existing objects, temporary
     const ScPatternAttr* pOld2;
 

cactus-bm/libreoffice-core

commit 6fb73f6de3cbc1649ecb3d1f907b478bdf59d76d

Fix presumed typo
...introduced in 6cb400f41df0dd108cdb4b4d3ec6656844814147 "store the SfxItemSet
inside SfxSetItem by value".  It appears to make more sense here to check that
an SfxItemSet is not an SfxAllItemSet, than to check that an SfxSetItem is not
an SfxAllItemSet.

Change-Id: I292c2a88910db9852cf8ba7c61999c5f281ed14a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/143119
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svl/source/items/sitem.cxx
@@ -44,7 +44,7 @@ SfxSetItem::SfxSetItem( const SfxSetItem& rCopy, SfxItemPool *pPool ) :
     SfxPoolItem(rCopy),
     maSet(rCopy.maSet.CloneAsValue(true, pPool))
 {
-    assert(!dynamic_cast<const SfxAllItemSet*>(&rCopy) && "cannot handle SfxAllItemSet here");
+    assert(!dynamic_cast<const SfxAllItemSet*>(&rCopy.maSet) && "cannot handle SfxAllItemSet here");
 }
 
 