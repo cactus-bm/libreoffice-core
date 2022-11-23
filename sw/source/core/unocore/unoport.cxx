LibreOffice/core

commit d57ad60d8ed5279a3366386553c5cefb7ae8b5bf

unique_ptr->optional in SwXTextPortion
Change-Id: I6b55ed9fb907473ff2e44bfa0bd5ccced750ca60
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/138158
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/unocore/unoport.cxx
@@ -100,25 +100,28 @@ SwXTextPortion::SwXTextPortion(
     : m_pPropSet(aSwMapProvider.GetPropertySet(
                     PROPERTY_MAP_TEXTPORTION_EXTENSIONS))
     , m_xParentText(std::move(xParent))
-    , m_pRubyText   ( bIsEnd ? nullptr : new uno::Any )
-    , m_pRubyStyle  ( bIsEnd ? nullptr : new uno::Any )
-    , m_pRubyAdjust ( bIsEnd ? nullptr : new uno::Any )
-    , m_pRubyIsAbove( bIsEnd ? nullptr : new uno::Any )
-    , m_pRubyPosition( bIsEnd ? nullptr : new uno::Any )
     , m_pFrameFormat(nullptr)
     , m_ePortionType( bIsEnd ? PORTION_RUBY_END : PORTION_RUBY_START )
     , m_bIsCollapsed(false)
 {
+    if (!bIsEnd)
+    {
+        m_oRubyText.emplace();
+        m_oRubyStyle.emplace();
+        m_oRubyAdjust.emplace();
+        m_oRubyIsAbove.emplace();
+        m_oRubyPosition.emplace();
+    }
     init( pPortionCursor);
 
     if (!bIsEnd)
     {
         const SfxPoolItem& rItem = rAttr.GetAttr();
-        rItem.QueryValue(*m_pRubyText);
-        rItem.QueryValue(*m_pRubyStyle, MID_RUBY_CHARSTYLE);
-        rItem.QueryValue(*m_pRubyAdjust, MID_RUBY_ADJUST);
-        rItem.QueryValue(*m_pRubyIsAbove, MID_RUBY_ABOVE);
-        rItem.QueryValue(*m_pRubyPosition, MID_RUBY_POSITION);
+        rItem.QueryValue(*m_oRubyText);
+        rItem.QueryValue(*m_oRubyStyle, MID_RUBY_CHARSTYLE);
+        rItem.QueryValue(*m_oRubyAdjust, MID_RUBY_ADJUST);
+        rItem.QueryValue(*m_oRubyIsAbove, MID_RUBY_ABOVE);
+        rItem.QueryValue(*m_oRubyPosition, MID_RUBY_POSITION);
     }
 }
 
@@ -357,17 +360,17 @@ void SwXTextPortion::GetPropertyValue(
         break;
         case RES_TXTATR_CJK_RUBY:
         {
-            const uno::Any* pToSet = nullptr;
+            const std::optional<uno::Any>* pToSet = nullptr;
             switch(rEntry.nMemberId)
             {
-                case MID_RUBY_TEXT :    pToSet = m_pRubyText.get();   break;
-                case MID_RUBY_ADJUST :  pToSet = m_pRubyAdjust.get(); break;
-                case MID_RUBY_CHARSTYLE:pToSet = m_pRubyStyle.get();  break;
-                case MID_RUBY_ABOVE :   pToSet = m_pRubyIsAbove.get();break;
-                case MID_RUBY_POSITION: pToSet = m_pRubyPosition.get();break;
+                case MID_RUBY_TEXT :    pToSet = &m_oRubyText;   break;
+                case MID_RUBY_ADJUST :  pToSet = &m_oRubyAdjust; break;
+                case MID_RUBY_CHARSTYLE:pToSet = &m_oRubyStyle;  break;
+                case MID_RUBY_ABOVE :   pToSet = &m_oRubyIsAbove;break;
+                case MID_RUBY_POSITION: pToSet = &m_oRubyPosition;break;
             }
             if(pToSet)
-                rVal = *pToSet;
+                rVal = **pToSet;
         }
         break;
         default:

sw/source/core/inc/unoport.hxx
@@ -111,11 +111,11 @@ private:
         m_xMeta;
     css::uno::Reference<css::text::XTextContent> m_xLineBreak;
     css::uno::Reference<css::text::XTextContent> m_xContentControl;
-    std::unique_ptr< css::uno::Any > m_pRubyText;
-    std::unique_ptr< css::uno::Any > m_pRubyStyle;
-    std::unique_ptr< css::uno::Any > m_pRubyAdjust;
-    std::unique_ptr< css::uno::Any > m_pRubyIsAbove;
-    std::unique_ptr< css::uno::Any > m_pRubyPosition;
+    std::optional< css::uno::Any > m_oRubyText;
+    std::optional< css::uno::Any > m_oRubyStyle;
+    std::optional< css::uno::Any > m_oRubyAdjust;
+    std::optional< css::uno::Any > m_oRubyIsAbove;
+    std::optional< css::uno::Any > m_oRubyPosition;
     sw::UnoCursorPointer m_pUnoCursor;
 
     SwFrameFormat*                  m_pFrameFormat;

cactus-bm/libreoffice-core

commit 448fa0556414f9b1fa150ce0cae1e205bdbd2172

tdf#152187: CRASH when opening Properties tab of Text Portion in UNO Inspector
relevant part of bt:
5  0x00007fa6fa5cdc0b in std::_Optional_base_impl<com::sun::star::uno::Any, std::_Optional_base<com::sun::star::uno::Any, false, false> >::_M_get() const (this=0x558f064c4508)
    at /usr/bin/../lib/gcc/x86_64-linux-gnu/12/../../../../include/c++/12/optional:484
6  0x00007fa6fa5ccf95 in std::optional<com::sun::star::uno::Any>::operator*() const & (this=0x558f064c4508) at /usr/bin/../lib/gcc/x86_64-linux-gnu/12/../../../../include/c++/12/optional:970
7  0x00007fa6fa5c8c3c in SwXTextPortion::GetPropertyValue(com::sun::star::uno::Any&, SfxItemPropertyMapEntry const&, SwUnoCursor*, std::unique_ptr<SfxItemSet, std::default_delete<SfxItemSet> >&)
    (this=0x558f064c43d0, rVal=uno::Any(void), rEntry=..., pUnoCursor=0x558f06515600, pSet=std::unique_ptr<SfxItemSet> = {...}) at sw/source/core/unocore/unoport.cxx:373
8  0x00007fa6fa5c90e8 in SwXTextPortion::GetPropertyValues_Impl(com::sun::star::uno::Sequence<rtl::OUString> const&) (this=0x558f064c43d0, rPropertyNames=uno::Sequence of length 1 = {...})
    at sw/source/core/unocore/unoport.cxx:414
9  0x00007fa6fa5c925f in SwXTextPortion::getPropertyValue(rtl::OUString const&) (this=0x558f064c43d0, rPropertyName="RubyAdjust") at sw/source/core/unocore/unoport.cxx:425
10 0x00007fa6fa5c940f in non-virtual thunk to SwXTextPortion::getPropertyValue(rtl::OUString const&) () at /home/julien/lo/libreoffice/instdir/program/../program/libswlo.so

Regression from d57ad60d8ed5279a3366386553c5cefb7ae8b5bf
"unique_ptr->optional in SwXTextPortion"
(in August 2022)

Change-Id: I9c4c5bbbcb439cb3105654044dd8a571d39ff513
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/143191
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

sw/source/core/unocore/unoport.cxx
@@ -369,7 +369,7 @@ void SwXTextPortion::GetPropertyValue(
                 case MID_RUBY_ABOVE :   pToSet = &m_oRubyIsAbove;break;
                 case MID_RUBY_POSITION: pToSet = &m_oRubyPosition;break;
             }
-            if(pToSet)
+            if(pToSet && *pToSet)
                 rVal = **pToSet;
         }
         break;