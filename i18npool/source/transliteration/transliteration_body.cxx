LibreOffice/core

commit c7551e8a46e2f9f8142aa7921a0494221ae096e8

speedup CharacterClassificationImpl::toUpper
remove empty sequence creation in CharacterClassificationImpl::toUpper,
rather pass a pointer, so it can be nullptr.
Which results in a fair degree of cascading change.

Change-Id: Ie56d49dc71480195c1807764b0d5124f0019f30b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122183
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

i18npool/source/transliteration/transliteration_body.cxx
@@ -88,7 +88,7 @@ static MappingType lcl_getMappingTypeForToggleCase( MappingType nMappingType, sa
 OUString
 Transliteration_body::transliterateImpl(
     const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount,
-    Sequence< sal_Int32 >& offset, bool useOffset)
+    Sequence< sal_Int32 >* pOffset)
 {
     const sal_Unicode *in = inStr.getStr() + startPos;
 
@@ -113,7 +113,7 @@ Transliteration_body::transliterateImpl(
     sal_Int32 j = 0;
     // Two different blocks to eliminate the if(useOffset) condition inside the loop.
     // Yes, on massive use even such small things do count.
-    if ( useOffset )
+    if ( pOffset )
     {
         std::vector<sal_Int32> aVec;
         aVec.reserve(std::max<sal_Int32>(nLocalBuf, nCount) * NMAPPINGMAX);
@@ -129,7 +129,7 @@ Transliteration_body::transliterateImpl(
             j += map.nmap;
         }
 
-        offset = comphelper::containerToSequence(aVec);
+        *pOffset = comphelper::containerToSequence(aVec);
     }
     else
     {
@@ -173,9 +173,9 @@ Transliteration_body::transliterateChar2Char( sal_Unicode inChar )
 
 OUString
 Transliteration_body::foldingImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount,
-    Sequence< sal_Int32 >& offset, bool useOffset)
+    Sequence< sal_Int32 >* pOffset)
 {
-    return transliterateImpl(inStr, startPos, nCount, offset, useOffset);
+    return transliterateImpl(inStr, startPos, nCount, pOffset);
 }
 
 Transliteration_casemapping::Transliteration_casemapping()
@@ -189,7 +189,8 @@ void
 Transliteration_casemapping::setMappingType( const MappingType rMappingType, const Locale& rLocale )
 {
     nMappingType = rMappingType;
-    aLocale = rLocale;
+    if (aLocale != rLocale)
+        aLocale = rLocale;
 }
 
 Transliteration_u2l::Transliteration_u2l()
@@ -228,7 +229,7 @@ Transliteration_titlecase::Transliteration_titlecase()
 static OUString transliterate_titlecase_Impl(
     const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount,
     const Locale &rLocale,
-    Sequence< sal_Int32 >& offset )
+    Sequence< sal_Int32 >* pOffset )
 {
     const OUString aText( inStr.copy( startPos, nCount ) );
 
@@ -255,10 +256,10 @@ static OUString transliterate_titlecase_Impl(
         // The rest of the text should just become lowercase.
         aRes = xCharClassImpl->toTitle( aResolvedLigature, 0, nResolvedLen, rLocale ) +
                xCharClassImpl->toLower( aText, 1, aText.getLength() - 1, rLocale );
-        offset.realloc( aRes.getLength() );
+        pOffset->realloc( aRes.getLength() );
 
-        sal_Int32* pOffset = std::fill_n(offset.begin(), nResolvedLen, 0);
-        std::iota(pOffset, offset.end(), 1);
+        sal_Int32* pOffsetInt = std::fill_n(pOffset->begin(), nResolvedLen, 0);
+        std::iota(pOffsetInt, pOffset->end(), 1);
     }
     return aRes;
 }
@@ -267,9 +268,9 @@ static OUString transliterate_titlecase_Impl(
 // namely that startPos points to the first char of the word
 OUString Transliteration_titlecase::transliterateImpl(
     const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount,
-    Sequence< sal_Int32 >& offset, bool )
+    Sequence< sal_Int32 >* pOffset )
 {
-    return transliterate_titlecase_Impl( inStr, startPos, nCount, aLocale, offset );
+    return transliterate_titlecase_Impl( inStr, startPos, nCount, aLocale, pOffset );
 }
 
 Transliteration_sentencecase::Transliteration_sentencecase()
@@ -283,9 +284,9 @@ Transliteration_sentencecase::Transliteration_sentencecase()
 // namely that startPos points to the first word (NOT first char!) in the sentence
 OUString Transliteration_sentencecase::transliterateImpl(
     const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount,
-    Sequence< sal_Int32 >& offset, bool )
+    Sequence< sal_Int32 >* pOffset )
 {
-    return transliterate_titlecase_Impl( inStr, startPos, nCount, aLocale, offset );
+    return transliterate_titlecase_Impl( inStr, startPos, nCount, aLocale, pOffset );
 }
 
 }

i18npool/inc/textToPronounce_zh.hxx
@@ -42,7 +42,7 @@ public:
         virtual ~TextToPronounce_zh() override;
 
         OUString
-        foldingImpl(const OUString & inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 > & offset, bool useOffset) override;
+        foldingImpl(const OUString & inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >* pOffset) override;
 
         sal_Int16 SAL_CALL getType() override;
 

i18npool/inc/transliteration_Ignore.hxx
@@ -33,7 +33,7 @@ class transliteration_Ignore : public transliteration_commonclass
 {
 public:
         virtual OUString
-        foldingImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) override;
+        foldingImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >* pOffset ) override;
 
         // This method is shared.
         sal_Bool SAL_CALL
@@ -49,7 +49,7 @@ public:
         sal_Int16 SAL_CALL getType(  ) override;
 
         OUString
-        transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) override;
+        transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >* pOffset ) override;
 
         virtual sal_Unicode SAL_CALL
         transliterateChar2Char( sal_Unicode inChar) override;
@@ -98,7 +98,7 @@ public:
     ignoreDiacritics_CTL();
 
     OUString
-    foldingImpl(const OUString& rInStr, sal_Int32 nStartPos, sal_Int32 nCount, css::uno::Sequence<sal_Int32>& rOffset, bool useOffset) override;
+    foldingImpl(const OUString& rInStr, sal_Int32 nStartPos, sal_Int32 nCount, css::uno::Sequence<sal_Int32>* pOffset) override;
 
     sal_Unicode SAL_CALL
     transliterateChar2Char(sal_Unicode nInChar) override;
@@ -117,7 +117,7 @@ public:\
             implementationName = "com.sun.star.i18n.Transliteration.ignore"#name;\
         };\
         OUString foldingImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, \
-                css::uno::Sequence< sal_Int32 >& offset, bool useOffset) override; \
+                css::uno::Sequence< sal_Int32 >* pOffset) override; \
 };
 
 TRANSLITERATION_IGNORE(KiKuFollowedBySa_ja_JP)
@@ -138,7 +138,7 @@ public:\
             implementationName = "com.sun.star.i18n.Transliteration.ignore"#name;\
         };\
         OUString foldingImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, \
-                css::uno::Sequence< sal_Int32 >& offset, bool useOffset) override; \
+                css::uno::Sequence< sal_Int32 >* pOffset) override; \
         using transliteration_Ignore::transliterateRange;\
         css::uno::Sequence< OUString > SAL_CALL transliterateRange( const OUString& str1, \
                 const OUString& str2 ) override; \

i18npool/inc/transliteration_Numeric.hxx
@@ -26,7 +26,7 @@ namespace i18npool {
 class transliteration_Numeric : public transliteration_commonclass {
 public:
         virtual OUString
-        transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) override;
+        transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >* pOffset ) override;
 
         virtual sal_Unicode SAL_CALL
         transliterateChar2Char( sal_Unicode inChar) override;
@@ -35,7 +35,7 @@ public:
         virtual sal_Int16 SAL_CALL getType(  ) override;
 
         virtual OUString
-        foldingImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) override;
+        foldingImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >* pOffset ) override;
 
         virtual sal_Bool SAL_CALL
         equals( const OUString& str1, sal_Int32 pos1, sal_Int32 nCount1, sal_Int32& nMatch1, const OUString& str2, sal_Int32 pos2, sal_Int32 nCount2, sal_Int32& nMatch2 ) override;
@@ -51,7 +51,7 @@ private:
         /// @throws css::uno::RuntimeException
         OUString
         transliterateBullet( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount,
-                             css::uno::Sequence< sal_Int32 >& offset, bool useOffset );
+                             css::uno::Sequence< sal_Int32 >* pOffset );
 };
 
 }

i18npool/inc/transliteration_OneToOne.hxx
@@ -31,7 +31,7 @@ class transliteration_OneToOne : public transliteration_commonclass
 {
 public:
         OUString
-        transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) override;
+        transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >* pOffset ) override;
 
         sal_Unicode SAL_CALL
         transliterateChar2Char( sal_Unicode inChar) override;
@@ -40,7 +40,7 @@ public:
         sal_Int16 SAL_CALL getType() override;
 
         OUString
-        foldingImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) override;
+        foldingImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >* pOffset ) override;
 
         sal_Bool SAL_CALL
         equals( const OUString& str1, sal_Int32 pos1, sal_Int32 nCount1, sal_Int32& nMatch1,
@@ -60,7 +60,7 @@ class name final : public transliteration_OneToOne \
 public: \
     name (); \
     OUString \
-    transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) \
+    transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >* pOffset ) \
      override; \
     sal_Unicode SAL_CALL \
     transliterateChar2Char( sal_Unicode inChar) \
@@ -76,21 +76,21 @@ class halfwidthToFullwidth final : public transliteration_OneToOne
 public:
     halfwidthToFullwidth();
     OUString
-    transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) override;
+    transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >* pOffset ) override;
 };
 class HALFWIDTHKATAKANA_FULLWIDTHKATAKANA final : public transliteration_OneToOne
 {
 public:
     HALFWIDTHKATAKANA_FULLWIDTHKATAKANA();
     OUString
-    transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) override;
+    transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >* pOffset ) override;
 };
 class HALFWIDTH_FULLWIDTH_LIKE_JIS final : public transliteration_OneToOne
 {
 public:
     HALFWIDTH_FULLWIDTH_LIKE_JIS();
     OUString
-    transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) override;
+    transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >* pOffset ) override;
 };
 
 #undef TRANSLITERATION_ONETOONE

i18npool/inc/transliteration_body.hxx
@@ -33,7 +33,7 @@ public:
     sal_Int16 SAL_CALL getType() override;
 
     OUString transliterateImpl(const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount,
-        css::uno::Sequence< sal_Int32 >& offset, bool useOffset) override;
+        css::uno::Sequence< sal_Int32 >* pOffset) override;
 
         OUString SAL_CALL
         transliterateChar2String( sal_Unicode inChar) override;
@@ -42,7 +42,7 @@ public:
         transliterateChar2Char( sal_Unicode inChar) override;
 
     OUString foldingImpl(const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount,
-        css::uno::Sequence< sal_Int32 >& offset, bool useOffset) override;
+        css::uno::Sequence< sal_Int32 >* pOffset) override;
 
     sal_Bool SAL_CALL equals(
         const OUString& str1, sal_Int32 pos1, sal_Int32 nCount1, sal_Int32& nMatch1,
@@ -86,7 +86,7 @@ public:
     Transliteration_titlecase();
 
     virtual OUString transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount,
-                                        css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) override;
+                                        css::uno::Sequence< sal_Int32 >* pOffset ) override;
 };
 
 class Transliteration_sentencecase final : public Transliteration_body
@@ -95,7 +95,7 @@ public:
     Transliteration_sentencecase();
 
     virtual OUString transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount,
-                                        css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) override;
+                                        css::uno::Sequence< sal_Int32 >* pOffset ) override;
 };
 
 }

i18npool/inc/transliteration_commonclass.hxx
@@ -57,11 +57,11 @@ public:
 
         virtual OUString SAL_CALL
         transliterate( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset ) override final
-            { return transliterateImpl( inStr, startPos, nCount, offset, true ); }
+            { return transliterateImpl( inStr, startPos, nCount, &offset ); }
 
         virtual OUString SAL_CALL
         folding( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset) override final
-            { return foldingImpl( inStr, startPos, nCount, offset, true ); }
+            { return foldingImpl( inStr, startPos, nCount, &offset ); }
 
         // Methods in XExtendedTransliteration
         virtual OUString SAL_CALL
@@ -89,10 +89,10 @@ public:
         virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 protected:
         virtual OUString
-        transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) = 0;
+        transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >* pOffset ) = 0;
 
         virtual OUString
-        foldingImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >& offset, bool useOffset ) = 0;
+        foldingImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, css::uno::Sequence< sal_Int32 >* pOffset ) = 0;
 
         css::lang::Locale   aLocale;
         const char*         transliterationName;

i18npool/source/transliteration/fullwidthToHalfwidth.cxx
@@ -44,13 +44,13 @@ fullwidthToHalfwidth::fullwidthToHalfwidth()
  * The output string contains a transliterated string only, not whole string.
  */
 OUString
-fullwidthToHalfwidth::transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, Sequence< sal_Int32 >& offset, bool useOffset )
+fullwidthToHalfwidth::transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, Sequence< sal_Int32 >* pOffset )
 {
     // Decomposition: GA --> KA + voice-mark
-    const OUString& newStr = i18nutil::widthfolding::decompose_ja_voiced_sound_marks (inStr, startPos, nCount, offset, useOffset);
+    const OUString& newStr = i18nutil::widthfolding::decompose_ja_voiced_sound_marks (inStr, startPos, nCount, pOffset);
 
     // One to One mapping
-    return transliteration_OneToOne::transliterateImpl( newStr, 0, newStr.getLength(), offset, false);
+    return transliteration_OneToOne::transliterateImpl( newStr, 0, newStr.getLength(), pOffset);
 }
 
 sal_Unicode SAL_CALL
@@ -74,13 +74,13 @@ FULLWIDTHKATAKANA_HALFWIDTHKATAKANA::FULLWIDTHKATAKANA_HALFWIDTHKATAKANA()
  * Transliterate fullwidth katakana to halfwidth katakana.
  */
 OUString
-FULLWIDTHKATAKANA_HALFWIDTHKATAKANA::transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, Sequence< sal_Int32 >& offset, bool useOffset )
+FULLWIDTHKATAKANA_HALFWIDTHKATAKANA::transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, Sequence< sal_Int32 >* pOffset )
 {
     // Decomposition: GA --> KA + voice-mark
-    const OUString& newStr = i18nutil::widthfolding::decompose_ja_voiced_sound_marks (inStr, startPos, nCount, offset, useOffset);
+    const OUString& newStr = i18nutil::widthfolding::decompose_ja_voiced_sound_marks (inStr, startPos, nCount, pOffset);
 
     // One to One mapping
-    return transliteration_OneToOne::transliterateImpl( newStr, 0, newStr.getLength(), offset, false);
+    return transliteration_OneToOne::transliterateImpl( newStr, 0, newStr.getLength(), nullptr);
 }
 
 sal_Unicode SAL_CALL
@@ -104,13 +104,13 @@ FULLWIDTH_HALFWIDTH_LIKE_ASC::FULLWIDTH_HALFWIDTH_LIKE_ASC()
  * Transliterate fullwidth to halfwidth like Excel's ASC function.
  */
 OUString
-FULLWIDTH_HALFWIDTH_LIKE_ASC::transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, Sequence< sal_Int32 >& offset, bool useOffset )
+FULLWIDTH_HALFWIDTH_LIKE_ASC::transliterateImpl( const OUString& inStr, sal_Int32 startPos, sal_Int32 nCount, Sequence< sal_Int32 >* pOffset )
 {
     // Decomposition: GA --> KA + voice-mark
-    const OUString& newStr = i18nutil::widthfolding::decompose_ja_voiced_sound_marks (inStr, startPos, nCount, offset, useOffset);
+    const OUString& newStr = i18nutil::widthfolding::decompose_ja_voiced_sound_marks (inStr, startPos, nCount, pOffset);
 
     // One to One mapping
-    return transliteration_OneToOne::transliterateImpl( newStr, 0, newStr.getLength(), offset, false);
+    return transliteration_OneToOne::transliterateImpl( newStr, 0, newStr.getLength(), nullptr);
 }
 
 sal_Unicode SAL_CALL

cactus-bm/libreoffice-core

commit 1cb84f2d9b0b23178141fef1df30d6b8e4165fe4

tdf#152520: Crash when using TITLE_CASE/SENTENCE_CASE in Transliteration module
relevant part of bt:
7  0x00007f50c85b3796 in com::sun::star::uno::Sequence<int>::realloc(int) (this=0x0, nSize=11) at include/com/sun/star/uno/Sequence.hxx:215
8  0x00007f50c85d9240 in i18npool::transliterate_titlecase_Impl(std::basic_string_view<char16_t, std::char_traits<char16_t> >, int, int, com::sun::star::lang::Locale const&, com::sun::star::uno::Sequence<int>*) (inStr=u"AB cd. eF. ", startPos=0, nCount=11, rLocale=..., pOffset=0x0) at i18npool/source/transliteration/transliteration_body.cxx:267
9  0x00007f50c85d955b in i18npool::Transliteration_sentencecase::transliterateImpl(rtl::OUString const&, int, int, com::sun::star::uno::Sequence<int>*)
    (this=0x557834ea0bb0, inStr="AB cd. eF. ", startPos=0, nCount=11, pOffset=0x0) at i18npool/source/transliteration/transliteration_body.cxx:298
10 0x00007f50c85dcc10 in i18npool::transliteration_commonclass::transliterateString2String(rtl::OUString const&, int, int) (this=0x557834ea0bb0, inStr="AB cd. eF. ", startPos=0, nCount=11)
    at i18npool/source/transliteration/transliteration_commonclass.cxx:109
11 0x00007f50c85dcc5d in non-virtual thunk to i18npool::transliteration_commonclass::transliterateString2String(rtl::OUString const&, int, int) ()
    at i18npool/source/transliteration/transliteration_commonclass.cxx:109
12 0x00007f50c85e0d89 in i18npool::TransliterationImpl::transliterateString2String(rtl::OUString const&, int, int) (this=0x557834854dc0, inStr="AB cd. eF. ", startPos=0, nCount=11)
    at i18npool/source/transliteration/transliterationImpl.cxx:395

full bt here:
https://bugs.documentfoundation.org/attachment.cgi?id=184205

Regression from:
c7551e8a46e2f9f8142aa7921a0494221ae096e8 (speedup CharacterClassificationImpl::toUpper)

Change-Id: I40e383fc87213d88fbde50876f61947b04190a90
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144375
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

i18npool/source/transliteration/transliteration_body.cxx
@@ -264,11 +264,14 @@ static OUString transliterate_titlecase_Impl(
         // The rest of the text should just become lowercase.
         aRes = xCharClassImpl->toTitle( aResolvedLigature, 0, nResolvedLen, rLocale ) +
                xCharClassImpl->toLower( aText, 1, aText.getLength() - 1, rLocale );
-        pOffset->realloc( aRes.getLength() );
+        if (pOffset)
+        {
+            pOffset->realloc( aRes.getLength() );
 
-        auto [begin, end] = asNonConstRange(*pOffset);
-        sal_Int32* pOffsetInt = std::fill_n(begin, nResolvedLen, 0);
-        std::iota(pOffsetInt, end, 1);
+            auto [begin, end] = asNonConstRange(*pOffset);
+            sal_Int32* pOffsetInt = std::fill_n(begin, nResolvedLen, 0);
+            std::iota(pOffsetInt, end, 1);
+        }
     }
     return aRes;
 }