LibreOffice/core

commit eb85de8e6b61fb3fcb6c03ae0145f7fe5478bccf

sw: layout: if fly's anchor moves forward, move fly off SwPageFrame
The problem is that on Show Changes->Hide Changes->Show Changes
in a 311 page document, the fly "Grafik1" was initially on page 203
but ends up on page 204, with a fly-sized gap on page 194.

In a 25 page cut down version of the bugdoc, on layout action 659
the fly's anchor SwTextFrame moves from page 21 to page 22, but
the fly remains in the SwPageFrame's m_pSortedObjs, because it's
not the anchor frame itself that moves but a distant previous frame,
and page 21 goes valid.

0  SwFlowFrame::PasteTree(SwFrame*, SwLayoutFrame*, SwFrame*, SwFrame*) (pStart=0x57c9e30, pParent=0xba15c50, pSibling=0x5a0f920, pOldParent=0xb057690) at sw/source/core/layout/flowfrm.cxx:586
1  SwFlowFrame::MoveSubTree(SwLayoutFrame*, SwFrame*) (this=0x57c9f48, pParent=0xba15c50, pSibling=0x5a0f920) at sw/source/core/layout/flowfrm.cxx:677
2  SwFlowFrame::MoveFwd(bool, bool, bool) (this=0x57c9f48, bMakePage=true, bPageBreak=false, bMoveAlways=false) at sw/source/core/layout/flowfrm.cxx:2061
3  SwContentFrame::MakeAll(OutputDevice*) (this=0x57c9e30) at sw/source/core/layout/calcmove.cxx:1831
4  SwFrame::OptPrepareMake() (this=0x57c9e30) at sw/source/core/layout/calcmove.cxx:399
5  SwFrame::OptCalc() const (this=0x57c9e30) at sw/source/core/inc/frame.hxx:1065
6  SwLayAction::FormatContent_(SwContentFrame const*, SwPageFrame const*) (this=0x7ffec0191b30, pContent=0x57c9e30, pPage=0xb9a1fd0) at sw/source/core/layout/layact.cxx:1833

In subsequent layout actions the anchor frame moves forward one
page at a time, until in action 665, when things get really interesting.

On page 24, the anchor text frame 582 is formatted for the first time,
and it moves the fly to page 24, after positioning it on the page.

2  SwPageFrame::MoveFly(SwFlyFrame*, SwPageFrame*) (this=0xb9a1fd0, pToMove=0x641d310, pDest=0x9125bb0) at sw/source/core/layout/flylay.cxx:972

sw/source/core/inc/flyfrms.hxx
@@ -167,6 +167,7 @@ class SwFlyAtContentFrame final: public SwFlyFreeFrame
         #i28701#
     */
     virtual void RegisterAtCorrectPage() override;
+    virtual void RegisterAtPage(SwPageFrame &) override;
     virtual void SwClientNotify(const SwModify&, const SfxHint&) override;
 
 public:

sw/inc/anchoreddrawobject.hxx
@@ -94,6 +94,7 @@ class SwAnchoredDrawObject final : public SwAnchoredObject
             page frame
         */
         virtual void RegisterAtCorrectPage() override;
+        virtual void RegisterAtPage(SwPageFrame &) override;
 
         virtual bool SetObjTop_( const SwTwips _nTop) override;
         virtual bool SetObjLeft_( const SwTwips _nLeft) override;

sw/inc/anchoredobject.hxx
@@ -295,6 +295,8 @@ class SW_DLLPUBLIC SwAnchoredObject
         /** method to invalidate position of the anchored object */
         virtual void InvalidateObjPos() = 0;
 
+        virtual void RegisterAtPage(SwPageFrame &) = 0;
+
         /** method to perform necessary invalidations for the positioning of
             objects, for whose the wrapping style influence has to be considered
             on the object positioning.

sw/source/core/inc/flyfrm.hxx
@@ -136,6 +136,7 @@ protected:
         page frame
     */
     virtual void RegisterAtCorrectPage() override;
+    virtual void RegisterAtPage(SwPageFrame &) override;
 
     virtual bool SetObjTop_( const SwTwips _nTop ) override;
     virtual bool SetObjLeft_( const SwTwips _nLeft ) override;

sw/source/core/inc/layact.hxx
@@ -111,7 +111,7 @@ class SwLayAction
 
     bool FormatLayout( OutputDevice* pRenderContext, SwLayoutFrame *, bool bAddRect = true );
     bool FormatLayoutTab( SwTabFrame *, bool bAddRect );
-    bool FormatContent( const SwPageFrame* pPage );
+    bool FormatContent(SwPageFrame * pPage);
     void FormatContent_( const SwContentFrame* pContent,
                        const SwPageFrame* pPage );
     bool IsShortCut( SwPageFrame *& );

sw/source/core/layout/anchoreddrawobject.cxx
@@ -919,10 +919,18 @@ void SwAnchoredDrawObject::RegisterAtCorrectPage()
     }
     if ( pPageFrame && GetPageFrame() != pPageFrame )
     {
-        if ( GetPageFrame() )
-            GetPageFrame()->RemoveDrawObjFromPage( *this );
-        pPageFrame->AppendDrawObjToPage( *this );
+        RegisterAtPage(*pPageFrame);
     }
+}
+
+void SwAnchoredDrawObject::RegisterAtPage(SwPageFrame & rPageFrame)
+{
+    assert(GetPageFrame() != &rPageFrame);
+    if (GetPageFrame())
+    {
+        GetPageFrame()->RemoveDrawObjFromPage( *this );
+    }
+    rPageFrame.AppendDrawObjToPage( *this );
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/source/core/layout/fly.cxx
@@ -2840,6 +2840,11 @@ void SwFlyFrame::RegisterAtCorrectPage()
     // default behaviour is to do nothing.
 }
 
+void SwFlyFrame::RegisterAtPage(SwPageFrame &)
+{
+    // default behaviour is to do nothing.
+}
+
 /** method to determine, if a <MakeAll()> on the Writer fly frame is possible
 
     OD 2004-05-11 #i28701#

sw/source/core/layout/flycnt.cxx
@@ -1429,10 +1429,20 @@ void SwFlyAtContentFrame::RegisterAtCorrectPage()
     }
     if ( pPageFrame && GetPageFrame() != pPageFrame )
     {
-        if ( GetPageFrame() )
-            GetPageFrame()->MoveFly( this, pPageFrame );
-        else
-            pPageFrame->AppendFlyToPage( this );
+        RegisterAtPage(*pPageFrame);
+    }
+}
+
+void SwFlyAtContentFrame::RegisterAtPage(SwPageFrame & rPageFrame)
+{
+    assert(GetPageFrame() != &rPageFrame);
+    if (GetPageFrame())
+    {
+        GetPageFrame()->MoveFly( this, &rPageFrame );
+    }
+    else
+    {
+        rPageFrame.AppendFlyToPage( this );
     }
 }
 

cactus-bm/libreoffice-core

commit 4bf924d47711a8441dac36de87eaa2c76bfd34db

tdf#146875 sw: move also at-frame flys off SwPageFrame
The bugdoc contains an at-frame anchored fly on page 77, which is
anchored to an at-page anchored fly.

While layouting page 74, the fly is moved to page 77, and the content on
page 74 invalidated.

Unexpectedly, calling RegisterAtPage on a SwFlyLayFrame does absolutely
nothing, as it calls the base class SwFlyFrame, so the fly stays on page
74.

Thus, page 74 never actually becomes valid, and later pages are never
layouted.

(regression from commit eb85de8e6b61fb3fcb6c03ae0145f7fe5478bccf)

Change-Id: I4fcbed4daf83f9e554de5a877a40b2c2419c0f2f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/144631
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/inc/flyfrms.hxx
@@ -150,6 +150,8 @@ public:
     // #i28701#
 
     SwFlyLayFrame( SwFlyFrameFormat*, SwFrame*, SwFrame *pAnchor );
+
+    virtual void RegisterAtPage(SwPageFrame &) override;
     virtual void SwClientNotify(const SwModify&, const SfxHint&) override;
 };
 

sw/source/core/layout/flylay.cxx
@@ -720,6 +720,19 @@ SwFlyLayFrame::SwFlyLayFrame( SwFlyFrameFormat *pFormat, SwFrame* pSib, SwFrame 
     m_bLayout = true;
 }
 
+void SwFlyLayFrame::RegisterAtPage(SwPageFrame & rPageFrame)
+{
+    assert(GetPageFrame() != &rPageFrame);
+    if (GetPageFrame())
+    {
+        GetPageFrame()->MoveFly( this, &rPageFrame );
+    }
+    else
+    {
+        rPageFrame.AppendFlyToPage( this );
+    }
+}
+
 // #i28701#
 
 void SwFlyLayFrame::SwClientNotify(const SwModify& rMod, const SfxHint& rHint)